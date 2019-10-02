d09dc784101f92219e8cf2801645d5b36de907d4
haldersaikat <45660476+haldersaikat@users.noreply.github.com>
2019-10-02 11:06:17 +0200

    PFP-8954 fikset problemer med tilbakekrevinghopp hvis behandling er allerede på tidligere steg (#64)

--- domenetjenester/behandling/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandling/impl/KravgrunnlagTjeneste.java
+++ domenetjenester/behandling/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandling/impl/KravgrunnlagTjeneste.java
@@ -6,8 +6,11 @@ import javax.inject.Inject;
 import no.nav.foreldrepenger.tilbakekreving.automatisk.gjenoppta.tjeneste.GjenopptaBehandlingTjeneste;
 import no.nav.foreldrepenger.tilbakekreving.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.foreldrepenger.tilbakekreving.behandlingskontroll.BehandlingskontrollTjeneste;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.BehandlingRepositoryProvider;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.BehandlingStegType;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.tilbakekreving.grunnlag.Kravgrunnlag431;
 import no.nav.foreldrepenger.tilbakekreving.grunnlag.KravgrunnlagRepository;
 import no.nav.foreldrepenger.tilbakekreving.grunnlag.kodeverk.KravStatusKode;
@@ -16,26 +19,33 @@ import no.nav.foreldrepenger.tilbakekreving.grunnlag.kodeverk.KravStatusKode;
 public class KravgrunnlagTjeneste {
 
     private KravgrunnlagRepository kravgrunnlagRepository;
+    private BehandlingRepository behandlingRepository;
     private GjenopptaBehandlingTjeneste gjenopptaBehandlingTjeneste;
     private BehandlingskontrollTjeneste behandlingskontrollTjeneste;
+
 
     KravgrunnlagTjeneste() {
         // For CDI
     }
 
     @Inject
-    public KravgrunnlagTjeneste(KravgrunnlagRepository kravgrunnlagRepository, GjenopptaBehandlingTjeneste gjenopptaBehandlingTjeneste, BehandlingskontrollTjeneste behandlingskontrollTjeneste) {
-        this.kravgrunnlagRepository = kravgrunnlagRepository;
+    public KravgrunnlagTjeneste(BehandlingRepositoryProvider repositoryProvider, GjenopptaBehandlingTjeneste gjenopptaBehandlingTjeneste,
+                                BehandlingskontrollTjeneste behandlingskontrollTjeneste) {
+        this.kravgrunnlagRepository = repositoryProvider.getGrunnlagRepository();
+        this.behandlingRepository = repositoryProvider.getBehandlingRepository();
         this.gjenopptaBehandlingTjeneste = gjenopptaBehandlingTjeneste;
         this.behandlingskontrollTjeneste = behandlingskontrollTjeneste;
     }
 
     public void lagreTilbakekrevingsgrunnlagFraØkonomi(Long behandlingId, Kravgrunnlag431 kravgrunnlag431) {
-        if (KravStatusKode.ENDRET.equals(kravgrunnlag431.getKravStatusKode())) {
-            BehandlingskontrollKontekst kontekst = behandlingskontrollTjeneste.initBehandlingskontroll(behandlingId);
+        Behandling behandling = behandlingRepository.hentBehandling(behandlingId);
+        if (KravStatusKode.ENDRET.equals(kravgrunnlag431.getKravStatusKode()) && behandlingskontrollTjeneste.erStegPassert(behandling, BehandlingStegType.FAKTA_FEILUTBETALING)) {
+            BehandlingskontrollKontekst kontekst = behandlingskontrollTjeneste.initBehandlingskontroll(behandling);
             //forutsatt at FPTILBAKE allrede har fått SPER melding for den behandlingen og sett behandling på vent med VenteÅrsak VENT_PÅ_TILBAKEKREVINGSGRUNNLAG
-            behandlingskontrollTjeneste.settAutopunktTilUtført(AksjonspunktDefinisjon.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG,kontekst);
-            behandlingskontrollTjeneste.behandlingTilbakeføringTilTidligereBehandlingSteg(kontekst, BehandlingStegType.FAKTA_FEILUTBETALING);
+            behandlingskontrollTjeneste.settAutopunktTilUtført(AksjonspunktDefinisjon.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG, kontekst);
+            if(behandlingskontrollTjeneste.erStegPassert(behandling, BehandlingStegType.FAKTA_FEILUTBETALING)){
+                behandlingskontrollTjeneste.behandlingTilbakeføringTilTidligereBehandlingSteg(kontekst, BehandlingStegType.FAKTA_FEILUTBETALING);
+            }
         }
         kravgrunnlagRepository.lagre(behandlingId, kravgrunnlag431);
         gjenopptaBehandlingTjeneste.fortsettBehandlingMedGrunnlag(behandlingId);
