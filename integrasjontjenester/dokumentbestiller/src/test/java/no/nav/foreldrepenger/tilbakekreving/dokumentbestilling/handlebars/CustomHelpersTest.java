6cca01ff8801fd9371d5be31cdbafe84e50e6146
Tore Langedal Endestad <Tore.Langedal.Endestad@nav.no>
2019-11-11 10:37:05 +0100

    NOJIRA fiks test

--- integrasjontjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/handlebars/CustomHelpersTest.java
+++ integrasjontjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/handlebars/CustomHelpersTest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 import org.junit.Test;
 
-class CustomHelpersTest {
+public class CustomHelpersTest {
     @Test
     public void skal_ha_riktig_tusenskille() {
         assertThat(medTusenskille(BigDecimal.valueOf(1), " ")).isEqualTo("1");
