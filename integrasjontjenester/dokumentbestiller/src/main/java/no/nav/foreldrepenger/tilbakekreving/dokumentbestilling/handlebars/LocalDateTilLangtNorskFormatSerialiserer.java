b3ed51715348eceafe7be4cd64c6c4077cf32189
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-01-28 11:48:58 +0100

    Byttet til DateTimeFormatter for å formattere dato

--- integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/handlebars/LocalDateTilLangtNorskFormatSerialiserer.java
+++ integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/handlebars/LocalDateTilLangtNorskFormatSerialiserer.java
@@ -2,9 +2,8 @@ package no.nav.foreldrepenger.tilbakekreving.dokumentbestilling.handlebars;
 
 
 import java.io.IOException;
-import java.sql.Date;
-import java.text.DateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -13,13 +12,13 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 public class LocalDateTilLangtNorskFormatSerialiserer extends JsonSerializer {
 
-    private static final DateFormat FORMATTER = DateFormat.getDateInstance(1, new Locale("no"));
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d. MMMM yyyy", new Locale("no"));
 
     @Override
     public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         if (o instanceof LocalDate) {
             LocalDate verdi = (LocalDate) o;
-            jsonGenerator.writeObject(FORMATTER.format(Date.valueOf(verdi)));
+            jsonGenerator.writeObject(FORMATTER.format(verdi));
         } else {
             throw new IllegalArgumentException("Serialiserer kan bare brukes for LocalDate");
         }
