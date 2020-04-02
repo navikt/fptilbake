c90bc50c5deaba7a7c1154a471fef6ba088c0111
Saikat Halder <saikat.halder@nav.no>
2020-04-02 11:06:38 +0200

    PFP-9297 Korrigerte vilkårresultat

--- kontrakter/src/main/java/no/nav/foreldrepenger/tilbakekreving/kontrakter/vedtak/UtvidetVilkårResultat.java
+++ kontrakter/src/main/java/no/nav/foreldrepenger/tilbakekreving/kontrakter/vedtak/UtvidetVilkårResultat.java
@@ -3,7 +3,7 @@ package no.nav.foreldrepenger.tilbakekreving.kontrakter.vedtak;
 public enum UtvidetVilkårResultat {
     FORELDET,
     FORSTO_BURDE_FORSTAATT,
-    FEIL_OPPLYSNINGER_FRA_BRUKER,
-    MANGELFULLE_OPPLYSNINGER_FRA_BRUKER,
+    FEIL_OPPLYSNINGER,
+    MANGELFULL_OPPLYSNING,
     GOD_TRO
 }
