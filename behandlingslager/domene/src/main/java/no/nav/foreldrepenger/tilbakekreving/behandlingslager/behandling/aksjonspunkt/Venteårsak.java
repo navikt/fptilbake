53991b871c3ad936bf52a94aa9530cc21c49d261
Geir Roger Moen <geir.roger.moen@nav.no>
2020-04-28 15:47:41 +0200

    PFP-9316 Ny Venteårsak og fjerner 2 gamle. Etter review

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/behandling/aksjonspunkt/Venteårsak.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/behandling/aksjonspunkt/Venteårsak.java
@@ -15,9 +15,7 @@ public class Venteårsak extends Kodeliste {
     public static final Venteårsak VENT_PÅ_TILBAKEKREVINGSGRUNNLAG = new Venteårsak("VENT_PÅ_TILBAKEKREVINGSGRUNNLAG"); //$NON-NLS-1$
 
     public static final Venteårsak AVVENTER_DOKUMENTASJON = new Venteårsak("AVV_DOK"); //$NON-NLS-1$
-    public static final Venteårsak VENT_PÅ_SCANNING = new Venteårsak("SCANN"); //$NON-NLS-1$
     public static final Venteårsak UTVIDET_TILSVAR_FRIST = new Venteårsak("UTV_TIL_FRIST"); //$NON-NLS-1$
-    public static final Venteårsak VENT_PÅ_ØKONOMI = new Venteårsak("VENT_PÅ_ØKONOMI"); //$NON-NLS-1$
     public static final Venteårsak ENDRE_TILKJENT_YTELSE = new Venteårsak("ENDRE_TILKJENT_YTELSE"); //$NON-NLS-1$
     public static final Venteårsak VENT_PÅ_MULIG_MOTREGNING = new Venteårsak("VENT_PÅ_MULIG_MOTREGNING"); //$NON-NLS-1$
 
@@ -28,7 +26,7 @@ public class Venteårsak extends Kodeliste {
     }
 
     public static boolean venterPåØkonomi(Venteårsak venteårsak) {
-        return VENT_PÅ_TILBAKEKREVINGSGRUNNLAG.equals(venteårsak) || VENT_PÅ_ØKONOMI.equals(venteårsak) || VENT_PÅ_MULIG_MOTREGNING.equals(venteårsak);
+        return VENT_PÅ_TILBAKEKREVINGSGRUNNLAG.equals(venteårsak) || VENT_PÅ_MULIG_MOTREGNING.equals(venteårsak);
     }
 
     public Venteårsak() {
