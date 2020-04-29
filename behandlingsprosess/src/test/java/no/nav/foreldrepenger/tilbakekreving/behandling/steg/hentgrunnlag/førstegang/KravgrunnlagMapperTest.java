2ed3d3cb1c3d02bd14227ce35facfe8efd17b220
Saikat Halder <saikat.halder@nav.no>
2020-04-29 15:33:46 +0200

    PFP-9317 ignorere posteringer med positiv ytel

--- behandlingsprosess/src/test/java/no/nav/foreldrepenger/tilbakekreving/behandling/steg/hentgrunnlag/førstegang/KravgrunnlagMapperTest.java
+++ behandlingsprosess/src/test/java/no/nav/foreldrepenger/tilbakekreving/behandling/steg/hentgrunnlag/førstegang/KravgrunnlagMapperTest.java
@@ -72,4 +72,31 @@ public class KravgrunnlagMapperTest extends FellesTestOppsett {
         assertThat(beløpYTEL.getSkattProsent()).isEqualByComparingTo(BigDecimal.valueOf(50));
     }
 
+    @Test
+    public void skal_ignorere_belop_postering_med_positiv_ytel() {
+        String xml = getInputXML("xml/kravgrunnlag_periode_POSITIV_YTEL.xml");
+        DetaljertKravgrunnlag input = KravgrunnlagXmlUnmarshaller.unmarshall(0L, xml);
+
+        Mockito.when(tpsAdapterMock.hentAktørIdForPersonIdent(PersonIdent.fra("12345678901"))).thenReturn(Optional.of(new AktørId(999999L)));
+
+        Kravgrunnlag431 oversatt = mapper.mapTilDomene(input);
+        assertThat(oversatt.getUtbetalesTilId()).isEqualTo("999999"); //aktørId
+        assertThat(oversatt.getGjelderVedtakId()).isEqualTo("999999"); //aktørId
+
+        assertThat(oversatt.getPerioder()).hasSize(1);
+        KravgrunnlagPeriode432 periode = oversatt.getPerioder().get(0);
+        assertThat(periode.getBeløpSkattMnd()).isEqualByComparingTo(BigDecimal.valueOf(2104));
+
+        List<KravgrunnlagBelop433> beløpene = periode.getKravgrunnlagBeloper433();
+        assertThat(beløpene).hasSize(2);
+        assertThat(beløpene.stream().filter(b -> b.getKlasseType().equals(KlasseType.FEIL)).findAny()).isNotEmpty();
+        KravgrunnlagBelop433 beløpYTEL = beløpene.stream().filter(b -> b.getKlasseType().equals(KlasseType.YTEL)).findFirst().orElseThrow();
+        assertThat(beløpYTEL.getKlasseType()).isEqualTo(KlasseType.YTEL);
+        assertThat(beløpYTEL.getKlasseKode()).isEqualTo(KlasseKode.FPATORD.getKode());
+        assertThat(beløpYTEL.getTilbakekrevesBelop()).isEqualByComparingTo(BigDecimal.valueOf(1794));
+        assertThat(beløpYTEL.getNyBelop()).isEqualByComparingTo(BigDecimal.valueOf(0));
+        assertThat(beløpYTEL.getOpprUtbetBelop()).isEqualByComparingTo(BigDecimal.valueOf(3270));
+        assertThat(beløpYTEL.getSkattProsent()).isEqualByComparingTo(BigDecimal.valueOf(43.9799));
+    }
+
 }
