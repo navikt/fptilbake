dc0ed870ac16b80bd833d31a789740cfbebcd0dc
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-05-20 11:31:18 +0200

    Fix regex for maskering av passord (ytelse og corner-case) (#465)

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/startupinfo/SystemPropertiesHelper.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/startupinfo/SystemPropertiesHelper.java
@@ -1,7 +1,5 @@
 package no.nav.foreldrepenger.tilbakekreving.web.app.startupinfo;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.SortedMap;
@@ -11,10 +9,10 @@ import java.util.regex.Pattern;
 
 class SystemPropertiesHelper {
 
-    private static final List<String> SECRET_NAME_PARTS = Arrays.asList("passord", "password", "passwd", "private", "key");
+    private static final Pattern SECRET_NAME_PARTS = Pattern.compile("passw?o?rd?|private|key", Pattern.CASE_INSENSITIVE);
     private static final String FILTERED_VALUE = "*****";
-    private static final Pattern SECRET_VERDI_REGEX = Pattern.compile("([^= ]*?(?:passord|password|passwd|private|key)[^=]*\\s*=\\s*)[^ ]*", Pattern.CASE_INSENSITIVE);
-    private static final String FILTER_PATTERN="$1*****";
+    private static final Pattern SECRET_VERDI_REGEX = Pattern.compile("((?:passw?o?rd?|private|key)[^=]*\\s*=\\s*)[^ ]*", Pattern.CASE_INSENSITIVE);
+    private static final String FILTER_PATTERN = "$1*****";
 
     private SystemPropertiesHelper() {
         // skjult
@@ -25,7 +23,6 @@ class SystemPropertiesHelper {
     }
 
     SortedMap<String, String> filteredSortedProperties() {
-
         Properties sysProps = System.getProperties();
         SortedMap<String, String> map = new TreeMap<>();
         for (String name : sysProps.stringPropertyNames()) {
@@ -36,7 +33,6 @@ class SystemPropertiesHelper {
     }
 
     SortedMap<String, String> filteredSortedEnvVars() {
-
         SortedMap<String, String> map = new TreeMap<>();
         map.putAll(System.getenv());
         filter(map);
@@ -47,19 +43,16 @@ class SystemPropertiesHelper {
         for (Map.Entry<String, String> entry : map.entrySet()) {
             if (containsSecretNamePart(entry.getKey())) {
                 entry.setValue(FILTERED_VALUE);
-            }
-            if (containsSecretNamePart(entry.getValue())) {
+            } else if (entry.getValue().indexOf('=') >= 0 && containsSecretNamePart(entry.getValue())) {
                 Matcher matcher = SECRET_VERDI_REGEX.matcher(entry.getValue());
-                String filtered = matcher.replaceFirst(FILTER_PATTERN);
+                String filtered = matcher.replaceAll(FILTER_PATTERN);
                 entry.setValue(filtered);
             }
         }
     }
 
     private static boolean containsSecretNamePart(String name) {
-        boolean containsSecretNamePart = SECRET_NAME_PARTS.stream()
-                .anyMatch(secrNamePart -> name.toLowerCase().contains(secrNamePart.toLowerCase()));
-        return containsSecretNamePart;
+        return SECRET_NAME_PARTS.matcher(name).find();
     }
 }
 
