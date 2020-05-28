0d70b1fe0a6c5102e9f3f170e2b50dcd82bd81f8
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-05-28 13:22:34 +0200

    Legger til prometheus for logging av JVM-stats

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/tjenester/ApplicationServiceStarterImpl.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/tjenester/ApplicationServiceStarterImpl.java
@@ -14,6 +14,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.prometheus.client.hotspot.DefaultExports;
 import no.nav.vedtak.apptjeneste.AppServiceHandler;
 
 @ApplicationScoped
@@ -33,6 +34,7 @@ public class ApplicationServiceStarterImpl implements ApplicationServiceStarter 
 
     @Override
     public void startServices() {
+        DefaultExports.initialize();
         serviceMap.forEach((key, value) -> {
             if (value.compareAndSet(false, true)) {
                 LOGGER.info("starter service: {}", key.getClass().getSimpleName());
