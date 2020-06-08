8348296fecb91d7522d1260a126894a748d20328
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-06-08 08:43:00 +0200

    PFP-8195 migrer db er eget steg

--- oracle/start-oracle.sh
+++ oracle/start-oracle.sh
@@ -1,8 +1,8 @@
 echo "Henter og starter Oracle image docker.pkg.github.com/navikt/fpsak-autotest/oracle-flattened"
-start=`date +%s`
+start=$(date +%s)
 docker-compose -f oracle/docker-compose.yml pull
 rm -f nohup.out
 docker-compose -f oracle/docker-compose.yml up oracle >nohup.out 2>&1 &
 sh -c 'tail -n +0 -f nohup.out | { sed "/Disconnected/q" && kill $$; }' || true
-end=`date +%s`
+end=$(date +%s)
 echo "Oracle started using $((end-start)) seconds" > "start-oracle.status"
