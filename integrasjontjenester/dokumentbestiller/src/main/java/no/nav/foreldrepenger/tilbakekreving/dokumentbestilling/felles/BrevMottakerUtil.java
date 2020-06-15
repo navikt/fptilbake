bc77686bd136ed5ced062d63e5bb77f41118abe9
Saikat Halder <saikat.halder@nav.no>
2020-06-15 16:45:18 +0200

    PFP-9401 Fikset mottakerNavn i brev

--- integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/felles/BrevMottakerUtil.java
+++ integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/felles/BrevMottakerUtil.java
@@ -4,9 +4,13 @@ import no.nav.foreldrepenger.tilbakekreving.dokumentbestilling.fritekstbrev.Brev
 
 public class BrevMottakerUtil {
 
+    BrevMottakerUtil(){
+        //privat konstruktor
+    }
+
     public static String getAnnenMottakerNavn(BrevMetadata brevMetadata){
         String mottakerNavn = brevMetadata.getMottakerAdresse().getMottakerNavn();
         String brukerNavn = brevMetadata.getSakspartNavn();
-        return mottakerNavn.equalsIgnoreCase(brukerNavn) ? brevMetadata.getVergeNavn() : brukerNavn;
+        return (mottakerNavn.equalsIgnoreCase(brukerNavn) || mottakerNavn.contains(brukerNavn)) ? brevMetadata.getVergeNavn() : brukerNavn;
     }
 }
