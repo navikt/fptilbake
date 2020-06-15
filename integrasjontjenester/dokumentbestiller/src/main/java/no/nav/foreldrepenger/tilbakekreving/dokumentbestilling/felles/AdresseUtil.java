36db47527d473390db0eb8290557a69ea070e225
Saikat Halder <saikat.halder@nav.no>
2020-06-15 15:30:37 +0200

    PFP-9401 endret for organisasjon adresse. Det kommer med landkode 'NO'

--- integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/felles/AdresseUtil.java
+++ integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/felles/AdresseUtil.java
@@ -1,15 +1,22 @@
 package no.nav.foreldrepenger.tilbakekreving.dokumentbestilling.felles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.aktør.Adresseinfo;
 
 public class AdresseUtil {
+    private static List<String> landskodeNorge = new ArrayList<>();
+    static {
+        landskodeNorge.add("NOR");
+        landskodeNorge.add("NO");
+    }
     private AdresseUtil() {
         //indrer instansiering
     }
 
     public static boolean erNorskAdresse(Adresseinfo adresse) {
-        String landskodeNorge = "NOR";
-        return adresse.getLand() != null && landskodeNorge.equalsIgnoreCase(adresse.getLand()) &&
+        return adresse.getLand() != null && landskodeNorge.contains(adresse.getLand()) &&
             adresse.getPostNr() != null;
     }
 
