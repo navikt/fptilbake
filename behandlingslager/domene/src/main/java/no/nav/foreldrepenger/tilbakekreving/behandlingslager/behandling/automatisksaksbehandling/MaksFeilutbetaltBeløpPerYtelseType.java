1caa1da369d8c3b6fcbb54f1c9b02f0b3797c87d
Saikat Halder <saikat.halder@nav.no>
2020-09-09 14:24:13 +0200

    PFP-9488 fikset review kommentar

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/behandling/automatisksaksbehandling/MaksFeilutbetaltBeløpPerYtelseType.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/behandling/automatisksaksbehandling/MaksFeilutbetaltBeløpPerYtelseType.java
@@ -1,23 +1,27 @@
 package no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.automatisksaksbehandling;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.fagsak.FagsakYtelseType;
 
 public class MaksFeilutbetaltBeløpPerYtelseType {
 
-    private static Map<FagsakYtelseType,BigDecimal> maksFeilutbetaltBeløpPerYtelseType = new HashMap<>();
+    MaksFeilutbetaltBeløpPerYtelseType(){
+        // for CDI
+    }
+
+    private static Map<FagsakYtelseType,BigDecimal> maksFeilutbetaltBeløpPerYtelseTypeMap = new EnumMap<>(FagsakYtelseType.class);
 
     static {
-        maksFeilutbetaltBeløpPerYtelseType.put(FagsakYtelseType.FORELDREPENGER, BigDecimal.valueOf(586l));
-        maksFeilutbetaltBeløpPerYtelseType.put(FagsakYtelseType.ENGANGSTØNAD, BigDecimal.valueOf(586l));
-        maksFeilutbetaltBeløpPerYtelseType.put(FagsakYtelseType.SVANGERSKAPSPENGER, BigDecimal.valueOf(586l));
+        maksFeilutbetaltBeløpPerYtelseTypeMap.put(FagsakYtelseType.FORELDREPENGER, BigDecimal.valueOf(586l));
+        maksFeilutbetaltBeløpPerYtelseTypeMap.put(FagsakYtelseType.ENGANGSTØNAD, BigDecimal.valueOf(586l));
+        maksFeilutbetaltBeløpPerYtelseTypeMap.put(FagsakYtelseType.SVANGERSKAPSPENGER, BigDecimal.valueOf(586l));
     }
 
     public static BigDecimal getMaksFeilutbetaltBeløp(FagsakYtelseType fagsakYtelseType) {
-        return maksFeilutbetaltBeløpPerYtelseType.get(fagsakYtelseType);
+        return maksFeilutbetaltBeløpPerYtelseTypeMap.get(fagsakYtelseType);
     }
 
 }
