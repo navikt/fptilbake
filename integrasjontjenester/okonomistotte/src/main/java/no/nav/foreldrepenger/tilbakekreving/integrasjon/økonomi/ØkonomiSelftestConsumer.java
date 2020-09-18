116b2a6155ea2a77845265dbef7146ddb13eced1
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-09-18 14:56:24 +0200

    Flytter okonomistotte til felles (og rydder i pom) (#786)
    * Flytter okonomistotte til felles (og rydder i pom)
    * Fjerner domenetjenster/bom-modul.Ikke nødvendig når vi har så få domenetjeneste-moduler

--- integrasjontjenester/okonomistotte/src/main/java/no/nav/foreldrepenger/tilbakekreving/integrasjon/økonomi/ØkonomiSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.foreldrepenger.tilbakekreving.integrasjon.økonomi;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface ØkonomiSelftestConsumer extends SelftestConsumer {
-}
