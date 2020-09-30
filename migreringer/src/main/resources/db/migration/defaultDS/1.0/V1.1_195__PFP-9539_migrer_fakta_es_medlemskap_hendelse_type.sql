8557d00a407b184343e1b5f8eb4d2792702a14d1
Saikat Halder <saikat.halder@nav.no>
2020-09-30 13:54:54 +0200

    PFP-9539 migrere ES fakta Meldemslkap hendelse type til ES_MEDLEMSKAP

---
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_195__PFP-9539_migrer_fakta_es_medlemskap_hendelse_type.sql
@@ -0,0 +1,6 @@
+UPDATE FAKTA_FEILUTBETALING_PERIODE SET HENDELSE_TYPE='ES_MEDLEMSKAP' where id=
+(select per.id from BEHANDLING beh
+inner join FAGSAK fagsak on beh.FAGSAK_ID = fagsak.ID
+inner join GR_FAKTA_FEILUTBETALING fakta on beh.id = fakta.BEHANDLING_ID
+inner join FAKTA_FEILUTBETALING_PERIODE per on per.fakta_feilutbetaling_id = fakta.FAKTA_FEILUTBETALING_ID
+where per.HENDELSE_TYPE='MEDLEMSKAP' and fagsak.YTELSE_TYPE='ES');
