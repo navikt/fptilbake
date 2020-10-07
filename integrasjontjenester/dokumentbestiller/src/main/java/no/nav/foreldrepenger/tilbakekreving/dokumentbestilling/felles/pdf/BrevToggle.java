8b8da7c5760581f2d496497d87cc4c66b9b472ff
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-10-07 09:16:18 +0200

    Bruk pdf-brev i k9-tilbake

--- integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/felles/pdf/BrevToggle.java
+++ integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/felles/pdf/BrevToggle.java
@@ -1,10 +1,13 @@
 package no.nav.foreldrepenger.tilbakekreving.dokumentbestilling.felles.pdf;
 
+import java.util.Objects;
+
 import no.nav.vedtak.util.env.Environment;
 
 public class BrevToggle {
 
     public static boolean brukDokprod() {
-        return Environment.current().isProd();
+        boolean erFptilbake = "fptilbake".equals(Objects.requireNonNull(System.getProperty("application.name")));
+        return erFptilbake && Environment.current().isProd();
     }
 }
