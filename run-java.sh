6fa76f9e6759b4b139169026eeccf69c00d8a462
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-10-15 22:35:52 +0200

    Forenkler run-java * truststore-opts settes uansett av init-script fra java-common * fjerner ubrukte opsjoner

--- run-java.sh
+++ run-java.sh
@@ -3,20 +3,7 @@ set -eu
 
 export JAVA_OPTS="${JAVA_OPTS:-} -Xmx1024m -Xms128m -Djava.security.egd=file:/dev/./urandom"
 
-if [ -r "${NAV_TRUSTSTORE_PATH:-}" ]; then
-    if ! echo "${NAV_TRUSTSTORE_PASSWORD}" | keytool -list -keystore ${NAV_TRUSTSTORE_PATH} > /dev/null;
-    then
-        echo Truststore is corrupt, or bad password
-        exit 1
-    fi
-
-    JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStore=${NAV_TRUSTSTORE_PATH}"
-    JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStorePassword=${NAV_TRUSTSTORE_PASSWORD}"
-fi
-
-# hvor skal gc log, heap dump etc kunne skrives til med Docker?
-export todo_JAVA_OPTS="${JAVA_OPTS} -XX:ErrorFile=./hs_err_pid<pid>.log -XX:HeapDumpPath=./java_pid<pid>.hprof -XX:-HeapDumpOnOutOfMemoryError -Xloggc:<filename>"
 export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.tilbakekreving.web.server.jetty.JettyServer"}
-export LOGBACK_CONFIG=${LOGBACK_CONFIG:-"./conf/logback.xml"}
+export CLASSPATH="app.jar:lib/*"
 
-exec java -cp "app.jar:lib/*"${EXTRA_CLASS_PATH:-""} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} -Dlogback.configurationFile=${LOGBACK_CONFIG?} -Dconf=${CONF:-"./conf"} -Dwebapp=${WEBAPP:-"./webapp"} -Di18n=${I18N:-"./i18n"} -Dapplication.name=${APP_NAME} ${STARTUP_CLASS?} $@
+exec java -cp ${CLASSPATH} ${DEFAULT_JVM_OPTS:-} ${JAVA_OPTS} -Dlogback.configurationFile="./conf/logback.xml" -Dapplication.name=${APP_NAME} ${STARTUP_CLASS?} $@
