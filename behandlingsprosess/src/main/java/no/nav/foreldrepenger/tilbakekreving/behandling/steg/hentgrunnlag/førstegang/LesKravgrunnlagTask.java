a20560027d48e3061fbaee1f092ebaa899ccb131
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2020-11-18 10:09:42 +0100

    PFP-9609  (#1042)
    * PFP-9609 endret kravgrunnlag.les batch-en til å koble grunnlaget med behandling basert på saksnummer
    
    * PFP-9609 fikset review kommentar og oppdaterte ekstern behandling med referanse fra grunnlag hvis det finnes i fagsystem

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandling/steg/hentgrunnlag/førstegang/LesKravgrunnlagTask.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandling/steg/hentgrunnlag/førstegang/LesKravgrunnlagTask.java
@@ -1,6 +1,8 @@
 package no.nav.foreldrepenger.tilbakekreving.behandling.steg.hentgrunnlag.førstegang;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -12,13 +14,16 @@ import no.nav.foreldrepenger.tilbakekreving.behandling.impl.KravgrunnlagTjeneste
 import no.nav.foreldrepenger.tilbakekreving.behandling.steg.hentgrunnlag.FellesTask;
 import no.nav.foreldrepenger.tilbakekreving.behandling.steg.hentgrunnlag.TaskProperty;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.Behandling;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.BehandlingType;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.ekstern.EksternBehandling;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.EksternBehandlingRepository;
 import no.nav.foreldrepenger.tilbakekreving.domene.typer.FagsystemId;
 import no.nav.foreldrepenger.tilbakekreving.domene.typer.Henvisning;
+import no.nav.foreldrepenger.tilbakekreving.domene.typer.Saksnummer;
 import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.FagsystemKlient;
+import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.dto.EksternBehandlingsinfoDto;
 import no.nav.foreldrepenger.tilbakekreving.grunnlag.Kravgrunnlag431;
 import no.nav.foreldrepenger.tilbakekreving.grunnlag.KravgrunnlagValidator;
 import no.nav.foreldrepenger.tilbakekreving.økonomixml.ØkonomiMottattXmlRepository;
@@ -43,8 +48,8 @@ public class LesKravgrunnlagTask extends FellesTask implements ProsessTaskHandle
     private ØkonomiMottattXmlRepository økonomiMottattXmlRepository;
     private KravgrunnlagTjeneste kravgrunnlagTjeneste;
     private KravgrunnlagMapper kravgrunnlagMapper;
-    private EksternBehandlingRepository eksternBehandlingRepository;
     private BehandlingRepository behandlingRepository;
+    private EksternBehandlingRepository eksternBehandlingRepository;
 
     LesKravgrunnlagTask() {
         //for CDI proxy
@@ -58,8 +63,8 @@ public class LesKravgrunnlagTask extends FellesTask implements ProsessTaskHandle
                                FagsystemKlient fagsystemKlient) {
         super(repositoryProvider.getGrunnlagRepository(), fagsystemKlient);
         this.økonomiMottattXmlRepository = økonomiMottattXmlRepository;
-        this.eksternBehandlingRepository = repositoryProvider.getEksternBehandlingRepository();
         this.behandlingRepository = repositoryProvider.getBehandlingRepository();
+        this.eksternBehandlingRepository = repositoryProvider.getEksternBehandlingRepository();
 
         this.kravgrunnlagTjeneste = kravgrunnlagTjeneste;
         this.kravgrunnlagMapper = kravgrunnlagMapper;
@@ -79,25 +84,19 @@ public class LesKravgrunnlagTask extends FellesTask implements ProsessTaskHandle
         boolean kravgrunnlagetErGyldig = validerKravgrunnlag(mottattXmlId, henvisning, saksnummer, kravgrunnlag);
         økonomiMottattXmlRepository.oppdaterMedHenvisningOgSaksnummer(henvisning, saksnummer, mottattXmlId);
 
-        Optional<EksternBehandling> behandlingKobling = hentKoblingTilInternBehandling(henvisning);
-        if (behandlingKobling.isPresent()) {
-            Long internId = behandlingKobling.get().getInternId();
-            logger.info("Leste kravgrunnlag med id={} henvisning={} internBehandlingId={}", mottattXmlId, henvisning, internId);
-
-            Behandling behandling = behandlingRepository.hentBehandling(internId);
-            if (!behandling.erAvsluttet()) {
-                kravgrunnlagTjeneste.lagreTilbakekrevingsgrunnlagFraØkonomi(internId, kravgrunnlag, kravgrunnlagetErGyldig);
-                økonomiMottattXmlRepository.opprettTilkobling(mottattXmlId);
-                logger.info("Behandling med internBehandlingId={} koblet med grunnlag id={}", internId, mottattXmlId);
-            } else {
-                logger.info("Behandling med internBehandlingId={} og henvisning={} er avsluttet, ikke koblet grunnlag med behandling", internId, henvisning);
-            }
-
+        Optional<Behandling> åpenTilbakekrevingBehandling = finnÅpenTilbakekrevingBehandling(saksnummer);
+        if (åpenTilbakekrevingBehandling.isPresent()) {
+            Behandling behandling = åpenTilbakekrevingBehandling.get();
+            long behandlingId = behandling.getId();
+            logger.info("Leste kravgrunnlag med id={} saksnummer={} internBehandlingId={}", mottattXmlId, saksnummer, behandlingId);
+            kravgrunnlagTjeneste.lagreTilbakekrevingsgrunnlagFraØkonomi(behandlingId, kravgrunnlag, kravgrunnlagetErGyldig);
+            økonomiMottattXmlRepository.opprettTilkobling(mottattXmlId);
+            logger.info("Behandling med internBehandlingId={} koblet med grunnlag id={}", behandlingId, mottattXmlId);
+            oppdaterHenvisningFraGrunnlag(behandling, saksnummer, henvisning);
         } else {
             validerBehandlingsEksistens(henvisning, saksnummer);
-            logger.info("Ignorerte kravgrunnlag med id={} henvisning={}. Fantes ikke tilbakekrevingsbehandling", mottattXmlId, henvisning);
+            logger.info("Ignorerte kravgrunnlag med id={} saksnummer={}. Fantes ikke en åpen tilbakekrevingsbehandling", mottattXmlId, saksnummer);
         }
-
     }
 
     private static boolean validerKravgrunnlag(Long mottattXmlId, Henvisning henvisning, String saksnummer, Kravgrunnlag431 kravgrunnlag) {
@@ -119,13 +118,38 @@ public class LesKravgrunnlagTask extends FellesTask implements ProsessTaskHandle
         }
     }
 
-    private Optional<EksternBehandling> hentKoblingTilInternBehandling(Henvisning henvisning) {
-        return eksternBehandlingRepository.hentFraHenvisning(henvisning);
+    private List<Behandling> hentBehandlingerForSaksnummer(String saksnummer) {
+        return behandlingRepository.hentAlleBehandlingerForSaksnummer(new Saksnummer(saksnummer));
+    }
+
+    private Optional<Behandling> finnÅpenTilbakekrevingBehandling(String saksnummer){
+        List<Behandling> behandlinger = hentBehandlingerForSaksnummer(saksnummer);
+        List<Behandling> åpneBehandlinger = behandlinger.stream()
+            .filter(beh -> BehandlingType.TILBAKEKREVING.equals(beh.getType()))
+            .filter(beh -> !beh.erAvsluttet()).collect(Collectors.toList());
+        if(åpneBehandlinger.size() > 1){
+            throw new IllegalArgumentException("Utvikler feil: Kan ikke ha flere åpne behandling for saksnummer="+ saksnummer);
+        }
+        return åpneBehandlinger.stream().findAny();
     }
 
     private void validerBehandlingsEksistens(Henvisning henvisning, String saksnummer) {
         if (!finnesYtelsesbehandling(saksnummer, henvisning)) {
-            throw LesKravgrunnlagTaskFeil.FACTORY.behandlingFinnesIkkeIFagsaksystemet(henvisning).toException();
+            throw LesKravgrunnlagTaskFeil.FACTORY.behandlingFinnesIkkeIFagsaksystemet(saksnummer, henvisning).toException();
+        }
+    }
+
+    private void oppdaterHenvisningFraGrunnlag(Behandling behandling, String saksnummer, Henvisning grunnlagHenvisning) {
+        List<EksternBehandlingsinfoDto> eksternBehandlinger = hentBehandlingerFraFagsystem(saksnummer);
+        Optional<EksternBehandlingsinfoDto> eksternBehandlingsinfoDto = eksternBehandlinger.stream()
+            .filter(eksternBehandling -> grunnlagHenvisning.equals(eksternBehandling.getHenvisning())).findFirst();
+        if (eksternBehandlingsinfoDto.isPresent()) {
+            logger.info("Oppdaterer EksternBehandling henvisning={} for behandlingId={}", grunnlagHenvisning, behandling.getId());
+            EksternBehandlingsinfoDto eksternBehandlingDto = eksternBehandlingsinfoDto.get();
+            EksternBehandling eksternBehandling = new EksternBehandling(behandling, eksternBehandlingDto.getHenvisning(), eksternBehandlingDto.getUuid());
+            eksternBehandlingRepository.lagre(eksternBehandling);
+        } else {
+            throw LesKravgrunnlagTaskFeil.FACTORY.behandlingFinnesIkkeIFagsaksystemet(saksnummer, grunnlagHenvisning).toException();
         }
     }
 
@@ -133,11 +157,10 @@ public class LesKravgrunnlagTask extends FellesTask implements ProsessTaskHandle
 
         LesKravgrunnlagTaskFeil FACTORY = FeilFactory.create(LesKravgrunnlagTaskFeil.class);
 
-        //FIXME k9-tilbake kan ikke hardkode fpsak i feilmeldingen
         @TekniskFeil(feilkode = "FPT-587195",
-            feilmelding = "Mottok et tilbakekrevingsgrunnlag fra Økonomi for en behandling som ikke finnes i fpsak. henvisning=%s. Kravgrunnlaget skulle kanskje til et annet system. Si i fra til Økonomi!",
+            feilmelding = "Mottok et tilbakekrevingsgrunnlag fra Økonomi for en behandling som ikke finnes i Fagsaksystemet for saksnummer=%s, henvisning=%s. Kravgrunnlaget skulle kanskje til et annet system. Si i fra til Økonomi!",
             logLevel = LogLevel.WARN)
-        Feil behandlingFinnesIkkeIFagsaksystemet(Henvisning henvisning);
+        Feil behandlingFinnesIkkeIFagsaksystemet(String saksnummer, Henvisning henvisning);
 
         @TekniskFeil(feilkode = "FPT-675363",
             feilmelding = "Mottok et tilbakekrevingsgrunnlag fra Økonomi med henvisning som ikke er i støttet format. henvisning=%s. Kravgrunnlaget skulle kanskje til et annet system. Si i fra til Økonomi!",
@@ -148,6 +171,7 @@ public class LesKravgrunnlagTask extends FellesTask implements ProsessTaskHandle
             feilmelding = "Mottok et ugyldig kravgrunnlag for saksnummer=%s henvisning=%s mottattXmlId=%s",
             logLevel = LogLevel.WARN)
         Feil ugyldigKravgrunnlag(String saksnummer, Henvisning henvisning, Long mottattXmlId, KravgrunnlagValidator.UgyldigKravgrunnlagException cause);
+
     }
 
 }
