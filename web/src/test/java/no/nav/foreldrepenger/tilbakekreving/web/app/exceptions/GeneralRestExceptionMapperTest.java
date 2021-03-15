3c0dcc34d5069a1f87efbc87d8c7074c4f36d190
Michal J. Sladek <michal.sladek@nav.no>
2021-03-15 21:11:44 +0100

    Java 15 implementasjon

--- web/src/test/java/no/nav/foreldrepenger/tilbakekreving/web/app/exceptions/GeneralRestExceptionMapperTest.java
+++ web/src/test/java/no/nav/foreldrepenger/tilbakekreving/web/app/exceptions/GeneralRestExceptionMapperTest.java
@@ -11,14 +11,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import ch.qos.logback.classic.Level;
+import no.nav.vedtak.exception.FunksjonellException;
+import no.nav.vedtak.exception.ManglerTilgangException;
+import no.nav.vedtak.exception.TekniskException;
 import no.nav.vedtak.exception.VLException;
-import no.nav.vedtak.feil.Feil;
-import no.nav.vedtak.feil.FeilFactory;
-import no.nav.vedtak.feil.LogLevel;
-import no.nav.vedtak.feil.deklarasjon.DeklarerteFeil;
-import no.nav.vedtak.feil.deklarasjon.FunksjonellFeil;
-import no.nav.vedtak.feil.deklarasjon.ManglerTilgangFeil;
-import no.nav.vedtak.feil.deklarasjon.TekniskFeil;
 import no.nav.vedtak.log.util.MemoryAppender;
 
 public class GeneralRestExceptionMapperTest {
@@ -49,9 +45,9 @@ public class GeneralRestExceptionMapperTest {
 
     @Test
     public void skalMappeManglerTilgangFeil() {
-        Feil manglerTilgangFeil = TestFeil.FACTORY.manglerTilgangFeil();
+        var manglerTilgangFeil = TestFeil.manglerTilgangFeil();
 
-        Response response = generalRestExceptionMapper.toResponse(new ApplicationException(manglerTilgangFeil.toException()));
+        Response response = generalRestExceptionMapper.toResponse(new ApplicationException(manglerTilgangFeil));
 
         assertThat(response.getStatus()).isEqualTo(403);
         assertThat(response.getEntity()).isInstanceOf(FeilDto.class);
@@ -64,9 +60,9 @@ public class GeneralRestExceptionMapperTest {
 
     @Test
     public void skalMappeFunksjonellFeil() {
-        Feil funksjonellFeil = TestFeil.FACTORY.funksjonellFeil();
+        var funksjonellFeil = TestFeil.funksjonellFeil();
 
-        Response response = generalRestExceptionMapper.toResponse(new ApplicationException(funksjonellFeil.toException()));
+        Response response = generalRestExceptionMapper.toResponse(new ApplicationException(funksjonellFeil));
 
         assertThat(response.getEntity()).isInstanceOf(FeilDto.class);
         FeilDto feilDto = (FeilDto) response.getEntity();
@@ -79,7 +75,7 @@ public class GeneralRestExceptionMapperTest {
 
     @Test
     public void skalMappeVLException() {
-        VLException vlException = TestFeil.FACTORY.tekniskFeil().toException();
+        VLException vlException = TestFeil.tekniskFeil();
 
         Response response = generalRestExceptionMapper.toResponse(new ApplicationException(vlException));
 
@@ -106,16 +102,18 @@ public class GeneralRestExceptionMapperTest {
         logSniffer.contains("en helt generell feil", Level.ERROR);
     }
 
-    interface TestFeil extends DeklarerteFeil {
-        TestFeil FACTORY = FeilFactory.create(TestFeil.class); // NOSONAR ok med konstant i interface her
+    private static class TestFeil {
 
-        @FunksjonellFeil(feilkode = "FUNK_FEIL", feilmelding = "en funksjonell feilmelding", løsningsforslag = "et løsningsforslag", logLevel = LogLevel.WARN)
-        Feil funksjonellFeil();
+        static FunksjonellException funksjonellFeil() {
+            return new FunksjonellException("FUNK_FEIL", "en funksjonell feilmelding", "et løsningsforslag");
+        }
 
-        @TekniskFeil(feilkode = "TEK_FEIL", feilmelding = "en teknisk feilmelding", logLevel = LogLevel.WARN)
-        Feil tekniskFeil();
+        static TekniskException tekniskFeil() {
+            return new TekniskException("TEK_FEIL", "en teknisk feilmelding");
+        }
 
-        @ManglerTilgangFeil(feilkode = "MANGLER_TILGANG_FEIL", feilmelding = "ManglerTilgangFeilmeldingKode", logLevel = LogLevel.WARN)
-        Feil manglerTilgangFeil();
+        static ManglerTilgangException manglerTilgangFeil() {
+            return new ManglerTilgangException("MANGLER_TILGANG_FEIL", "ManglerTilgangFeilmeldingKode");
+        }
     }
 }
