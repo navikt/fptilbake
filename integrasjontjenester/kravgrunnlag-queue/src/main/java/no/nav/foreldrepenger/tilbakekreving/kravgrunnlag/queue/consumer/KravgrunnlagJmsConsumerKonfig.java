466a7c1d3aba4e96b19ff5a8dcf8d945e7b4f0bb
Michal J. Sladek <mrsladek@users.noreply.github.com>
2021-07-05 14:47:32 +0200

    TFP-4472: Ny mq bruker for fptilbake i dev. Prod in progress. (#1485)

--- integrasjontjenester/kravgrunnlag-queue/src/main/java/no/nav/foreldrepenger/tilbakekreving/kravgrunnlag/queue/consumer/KravgrunnlagJmsConsumerKonfig.java
+++ integrasjontjenester/kravgrunnlag-queue/src/main/java/no/nav/foreldrepenger/tilbakekreving/kravgrunnlag/queue/consumer/KravgrunnlagJmsConsumerKonfig.java
@@ -1,8 +1,11 @@
 package no.nav.foreldrepenger.tilbakekreving.kravgrunnlag.queue.consumer;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import no.nav.foreldrepenger.konfig.Environment;
+import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.vedtak.felles.integrasjon.jms.BaseJmsKonfig;
 
 @Named("kravgrunnlagjmsconsumerkonfig")
@@ -13,19 +16,34 @@ public class KravgrunnlagJmsConsumerKonfig extends BaseJmsKonfig {
 
     private static final String INN_QUEUE_PREFIX = "fptilbake_kravgrunnlag";
 
+    private static final Environment ENV = Environment.current();
 
-    public KravgrunnlagJmsConsumerKonfig() {
+    private String mqBruker;
+    private String mqPassord;
+    private String appName;
+
+    private KravgrunnlagJmsConsumerKonfig() {
         super(INN_QUEUE_PREFIX);
+    }
+
+    @Inject
+    public KravgrunnlagJmsConsumerKonfig(@KonfigVerdi("systembruker.username") String bruker,
+                                         @KonfigVerdi("systembruker.password") String passord,
+                                         @KonfigVerdi("app.name") String appName) {
+        this();
+        this.mqBruker = bruker;
+        this.mqPassord = passord;
+        this.appName = appName;
     }
 
     @Override
     public String getQueueManagerUsername() {
-        return "srvappserver"; // TODO - hent fra konfig når ny MQ-konfig innføres i august/september
+        return (ENV.isDev() && "fptilbake".equals(appName)) ? mqBruker : "srvappserver";
     }
 
     @Override
     public String getQueueManagerPassword() {
-        return null; // TODO - hent fra vault e.l. når ny MQ-konfig innføres i august/september
+        return (ENV.isDev() && "fptilbake".equals(appName)) ? mqPassord : null;
     }
 
 }
--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/server/jetty/JmsKonfig.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/server/jetty/JmsKonfig.java
@@ -7,42 +7,36 @@ import org.eclipse.jetty.plus.jndi.EnvEntry;
 
 import com.ibm.mq.jms.MQConnectionFactory;
 import com.ibm.mq.jms.MQQueue;
+import com.ibm.msg.client.jms.JmsConstants;
 import com.ibm.msg.client.wmq.WMQConstants;
 import com.ibm.msg.client.wmq.compat.jms.internal.JMSC;
 
+import no.nav.foreldrepenger.konfig.Environment;
+
 class JmsKonfig {
 
-    private static final int MQ_TARGET_CLIENT = WMQConstants.WMQ_MESSAGE_BODY_MQ;
+    private final static Environment ENV = Environment.current();
 
     private JmsKonfig() { // Util class
     }
 
-    static void settOppJndiConnectionfactory(String jndiName, String queueManagerAlias, String channelAlias) throws JMSException, NamingException {
-        MQConnectionFactory mqConnectionFactory = createConnectionfactory(
-            getProperty(queueManagerAlias + ".hostname"),
-            Integer.parseUnsignedInt(getProperty(queueManagerAlias + ".port")),
-            getProperty(channelAlias + ".name"),
-            getProperty(queueManagerAlias + ".name"),
-            Boolean.getBoolean("mqGateway02.useSslOnJetty"));
+    static void settOppJndiConnectionfactory(String jndiName) throws JMSException, NamingException {
+        MQConnectionFactory mqConnectionFactory = createConnectionFactory(
+            ENV.getProperty("mqGateway02.hostname"),
+            ENV.getProperty("mqGateway02.port", Integer.class),
+            ENV.getProperty("mqGateway02.channel"),
+            ENV.getProperty("mqGateway02.name"));
+
         new EnvEntry(jndiName, mqConnectionFactory);
     }
 
-    static void settOppJndiMessageQueue(String jndiName, String queueAlias) throws NamingException, JMSException {
-        settOppJndiMessageQueue(jndiName, queueAlias, false);
-    }
+    static void settOppJndiMessageQueue(String jndiName, String queueNameProp) throws NamingException, JMSException {
+        MQQueue queue = new MQQueue(ENV.getProperty(queueNameProp));
 
-    static void settOppJndiMessageQueue(String jndiName, String queueAlias, boolean mqTargetClient) throws NamingException, JMSException {
-        MQQueue queue = new MQQueue(getProperty(queueAlias + ".queueName"));
-        if (mqTargetClient) {
-            queue.setMessageBodyStyle(MQ_TARGET_CLIENT);
-        }
         new EnvEntry(jndiName, queue);
     }
 
-    /**
-     * @param useSSL - FIXME (u139158): PFP-1176 Saneres når vi er over til å bruke SSL mot MQ overalt
-     */
-    private static MQConnectionFactory createConnectionfactory(String hostName, Integer port, String channel, String queueManagerName, boolean useSSL) throws JMSException {
+    private static MQConnectionFactory createConnectionFactory(String hostName, Integer port, String channel, String queueManagerName) throws JMSException {
         MQConnectionFactory connectionFactory = new MQConnectionFactory();
         connectionFactory.setHostName(hostName);
         connectionFactory.setPort(port);
@@ -51,8 +45,9 @@ class JmsKonfig {
         }
         connectionFactory.setQueueManager(queueManagerName);
         connectionFactory.setTransportType(JMSC.MQJMS_TP_CLIENT_MQ_TCPIP);
+        connectionFactory.setBooleanProperty(JmsConstants.USER_AUTHENTICATION_MQCSP, true);
 
-        if (useSSL) {
+        if (!ENV.isProd()) { // TODO sjekk om den fortsatt trenges - ble avviklet i fpsak - den brukes nok ikke til noe
             connectionFactory.setSSLCipherSuite("TLS_RSA_WITH_AES_128_CBC_SHA");
 
             // Denne trengs for at IBM MQ libs skal bruke/gjenkjenne samme ciphersuite navn som Oracle JRE:
@@ -61,13 +56,5 @@ class JmsKonfig {
         }
 
         return connectionFactory;
-    }
-
-    private static String getProperty(String key) {
-        String val = System.getProperty(key);
-        if (val == null) {
-            val = System.getenv(key.toUpperCase().replace('.', '_'));
-        }
-        return val;
     }
 }
