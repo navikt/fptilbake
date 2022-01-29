fe3a015a1be514c75f7a80a41dcd6787f4efa5fb
Michal J. Sladek <michal.sladek@nav.no>
2022-01-29 13:25:33 +0100

    Oppdatert konfig til MQ.

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/server/jetty/JettyServer.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/server/jetty/JettyServer.java
@@ -59,7 +59,7 @@ public class JettyServer extends AbstractJettyServer {
 
     protected void konfigurerJms() throws JMSException, NamingException {
         JmsKonfig.settOppJndiConnectionfactory("jms/ConnectionFactory");
-        JmsKonfig.settOppJndiMessageQueue("jms/QueueFptilbakeKravgrunnlag", "fptilbake_kravgrunnlag.queueName");
+        JmsKonfig.settOppJndiMessageQueue("jms/QueueFptilbakeKravgrunnlag", "fptilbake.kravgrunnlag.queueName");
     }
 
     @Override
