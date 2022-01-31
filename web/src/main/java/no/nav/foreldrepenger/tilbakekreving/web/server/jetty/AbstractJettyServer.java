223d60a0e2421396d714190e04ba2db1113bcbd6
Michal J. Sladek <michal.sladek@nav.no>
2022-01-31 16:13:23 +0100

    Fjerner bruk av jaspi-conf.xl

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/server/jetty/AbstractJettyServer.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/server/jetty/AbstractJettyServer.java
@@ -1,57 +1,26 @@
 package no.nav.foreldrepenger.tilbakekreving.web.server.jetty;
 
-import java.io.File;
 import java.io.IOException;
-import java.security.Security;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.security.auth.message.config.AuthConfigFactory;
-
-import org.apache.geronimo.components.jaspi.AuthConfigFactoryImpl;
-import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.jaas.JAASLoginService;
-import org.eclipse.jetty.plus.webapp.EnvConfiguration;
-import org.eclipse.jetty.plus.webapp.PlusConfiguration;
 import org.eclipse.jetty.rewrite.handler.RewriteHandler;
 import org.eclipse.jetty.security.ConstraintSecurityHandler;
 import org.eclipse.jetty.security.DefaultIdentityService;
 import org.eclipse.jetty.security.SecurityHandler;
 import org.eclipse.jetty.security.jaspi.JaspiAuthenticatorFactory;
-import org.eclipse.jetty.server.AbstractNetworkConnector;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.Configuration;
-import org.eclipse.jetty.webapp.WebAppConfiguration;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.webapp.WebInfConfiguration;
-import org.eclipse.jetty.webapp.WebXmlConfiguration;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 abstract class AbstractJettyServer {
 
-    /**
-     * @see AbstractNetworkConnector#getHost()
-     * @see ServerConnector#openAcceptChannel()
-     */
-    //TODO (u139158): Trenger vi egentlig å sette denne? Spec ser ut til å si at det er eq med null, settes den default til null eller binder den mot et interface?
-
-    protected static final String SERVER_HOST = "0.0.0.0";
-    /**
-     * nedstrippet sett med Jetty configurations for raskere startup.
-     */
-    protected static final Configuration[] CONFIGURATIONS = new Configuration[]{
-            new WebAppConfiguration(),
-            new WebInfConfiguration(),
-            new WebXmlConfiguration(),
-            new AnnotationConfiguration(),
-            new EnvConfiguration(),
-            new PlusConfiguration(),
-    };
     private AppKonfigurasjon appKonfigurasjon;
 
     public AbstractJettyServer(AppKonfigurasjon appKonfigurasjon) {
@@ -68,7 +37,6 @@ abstract class AbstractJettyServer {
     protected void konfigurer() throws Exception {
         konfigurerLogging();
         konfigurerMiljø();
-        konfigurerSikkerhet();
         konfigurerJndi();
     }
 
@@ -80,16 +48,6 @@ abstract class AbstractJettyServer {
     }
 
     protected abstract void konfigurerMiljø() throws Exception;
-
-    protected void konfigurerSikkerhet() {
-        Security.setProperty(AuthConfigFactory.DEFAULT_FACTORY_SECURITY_PROPERTY, AuthConfigFactoryImpl.class.getCanonicalName());
-
-        File jaspiConf = new File(System.getProperty("conf", "./conf") + "/jaspi-conf.xml");
-        if (!jaspiConf.exists()) {
-            throw new IllegalStateException("Missing required file: " + jaspiConf.getAbsolutePath());
-        }
-        System.setProperty("org.apache.geronimo.jaspic.configurationFile", jaspiConf.getAbsolutePath());
-    }
 
     protected abstract void konfigurerJndi() throws Exception;
 
@@ -113,7 +71,6 @@ abstract class AbstractJettyServer {
         List<Connector> connectors = new ArrayList<>();
         ServerConnector httpConnector = new ServerConnector(server, new HttpConnectionFactory(createHttpConfiguration()));
         httpConnector.setPort(appKonfigurasjon.getServerPort());
-        httpConnector.setHost(SERVER_HOST);
         connectors.add(httpConnector);
 
         return connectors;
@@ -137,7 +94,6 @@ abstract class AbstractJettyServer {
         webAppContext.setDescriptor(descriptor);
         webAppContext.setBaseResource(createResourceCollection());
         webAppContext.setContextPath(appKonfigurasjon.getContextPath());
-        webAppContext.setConfigurations(CONFIGURATIONS);
         webAppContext.setAttribute("org.eclipse.jetty.server.webapp.WebInfIncludeJarPattern", "^.*jersey-.*.jar$|^.*felles-.*.jar$");
         webAppContext.setSecurityHandler(createSecurityHandler());
 
@@ -159,10 +115,10 @@ abstract class AbstractJettyServer {
     protected abstract Resource createResourceCollection() throws IOException;
 
     private SecurityHandler createSecurityHandler() {
-        ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
+        var securityHandler = new ConstraintSecurityHandler();
         securityHandler.setAuthenticatorFactory(new JaspiAuthenticatorFactory());
 
-        JAASLoginService loginService = new JAASLoginService();
+        var loginService = new JAASLoginService();
         loginService.setName("jetty-login");
         loginService.setLoginModuleName("jetty-login");
         loginService.setIdentityService(new DefaultIdentityService());
