906fa03c96142beb289e5f56a0860cc994315a6c
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-04-28 21:34:47 +0200

    Fjerner 2 jettyDevServere og erstatte med 1. (#2018)

--- web/src/test/java/no/nav/foreldrepenger/tilbakekreving/web/server/jetty/JettyDevServer.java
+++ web/src/test/java/no/nav/foreldrepenger/tilbakekreving/web/server/jetty/JettyDevServer.java
@@ -2,7 +2,7 @@ package no.nav.foreldrepenger.tilbakekreving.web.server.jetty;
 
 import no.nav.foreldrepenger.konfig.Environment;
 
-public class JettyK9DevServer extends JettyServer {
+public class JettyDevServer extends JettyServer {
 
     private static final Environment ENV = Environment.current();
 
@@ -10,14 +10,14 @@ public class JettyK9DevServer extends JettyServer {
         jettyServer(args).bootStrap();
     }
 
-    static JettyK9DevServer jettyServer(String[] args) {
+    private static JettyDevServer jettyServer(String[] args) {
         if (args.length > 0) {
-            return new JettyK9DevServer(Integer.parseUnsignedInt(args[0]));
+            return new JettyDevServer(Integer.parseUnsignedInt(args[0]));
         }
-        return new JettyK9DevServer(ENV.getProperty("server.port", Integer.class, 8030));
+        return new JettyDevServer(ENV.getProperty("server.port", Integer.class, 8030));
     }
 
-    private JettyK9DevServer(int serverPort) {
+    private JettyDevServer(int serverPort) {
         super(serverPort);
     }
 }
