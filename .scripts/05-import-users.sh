689aa9659a8fc72e30c1dded5cfd9abd1b36003f
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-10-21 21:12:50 +0200

    Oppdater felles, fjern ldap konfig (#2171)

--- .scripts/05-import-users.sh
+++ .scripts/05-import-users.sh
@@ -29,15 +29,3 @@ then
   export SYSTEMBRUKER_PASSWORD=$(cat /var/run/secrets/nais.io/serviceuser/password)
   echo "Setting SYSTEMBRUKER_PASSWORD"
 fi
-
-if test -f /var/run/secrets/nais.io/ldap/username;
-then
-  export LDAP_USERNAME=$(cat /var/run/secrets/nais.io/ldap/username)
-  echo "Setting LDAP_USERNAME"
-fi
-
-if test -f /var/run/secrets/nais.io/ldap/password;
-then
-  export LDAP_PASSWORD=$(cat /var/run/secrets/nais.io/ldap/password)
-  echo "Setting LDAP_PASSWORD"
-fi
