2bd6ad2538876e7ec07260436d3cdb8bc9f53728
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2022-11-16 15:36:00 +0100

    Fix: JsonCreator så det er mulig å deserialisere

--- kontrakter/src/main/java/no/nav/foreldrepenger/tilbakekreving/kontrakter/sakshendelse/Periode.java
+++ kontrakter/src/main/java/no/nav/foreldrepenger/tilbakekreving/kontrakter/sakshendelse/Periode.java
@@ -2,6 +2,9 @@ package no.nav.foreldrepenger.tilbakekreving.kontrakter.sakshendelse;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -14,7 +17,9 @@ public class Periode {
     @JsonProperty(value = "tom")
     private LocalDate tom;
 
-    public Periode(LocalDate fom, LocalDate tom) {
+    @JsonCreator
+    public Periode(@JsonProperty(value = "periode", required = true) @NotNull LocalDate fom,
+                   @JsonProperty(value = "periode", required = true) @NotNull LocalDate tom) {
         this.fom = fom;
         this.tom = tom;
     }
