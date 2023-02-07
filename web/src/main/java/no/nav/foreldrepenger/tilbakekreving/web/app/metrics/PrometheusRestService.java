1ca9979d7284f09795078f063a92ed74164fcc8e
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-02-07 11:41:57 +0100

    Tilbake til prometheus simpleclient og ta med task gauges (#2412)

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/metrics/PrometheusRestService.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/metrics/PrometheusRestService.java
@@ -1,21 +1,43 @@
 package no.nav.foreldrepenger.tilbakekreving.web.app.metrics;
 
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.exporter.common.TextFormat;
 import io.swagger.v3.oas.annotations.Operation;
-import no.nav.vedtak.log.metrics.MetricsUtil;
+import no.nav.vedtak.felles.prosesstask.api.TaskMonitor;
 
 @Path("/metrics")
 @ApplicationScoped
 public class PrometheusRestService {
 
+    private static final Gauge TASK_GAUGE = Gauge.build()
+        .name("prosesstask_antall")
+        .labelNames("status")
+        .help("No tasks w/status.")
+        .register();
+
     @GET
-    @Operation(hidden = true)
+    @Operation(tags = "metrics", hidden = true)
     @Path("/prometheus")
-    public String prometheus() {
-        return MetricsUtil.scrape();
+    public Response prometheus() {
+        TaskMonitor.monitoredStatuses().forEach(s -> TASK_GAUGE.labels(TaskMonitor.statusLabel(s)).set(TaskMonitor.TASK_GAUGES.get(s).get()));
+        try (final Writer writer = new StringWriter()) {
+            TextFormat.writeFormat(TextFormat.CONTENT_TYPE_004, writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
+            return Response.ok().encoding("UTF-8").entity(writer.toString()).header("content-type", TextFormat.CONTENT_TYPE_004).build();
+        } catch (IOException e) {
+            //TODO logg?
+        }
+
+        return null;
     }
 }
