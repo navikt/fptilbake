ba76a22a512f1e89099d5c0d167272b1614daf6f
Michal J. Sladek <mrsladek@users.noreply.github.com>
2023-02-17 12:47:21 +0100

    Forenkler jetty scanning etter ressurser. (#2431)

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/tjenester/ApplicationContextListener.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/tjenester/ApplicationContextListener.java
@@ -1,24 +1,31 @@
 package no.nav.foreldrepenger.tilbakekreving.web.app.tjenester;
 
-import javax.enterprise.context.ApplicationScoped;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.inject.Inject;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
-@ApplicationScoped
+@WebListener
 public class ApplicationContextListener implements ServletContextListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ApplicationContextListener.class);
 
     @Inject
     private ApplicationServiceStarter applicationServiceStarter; //NOSONAR - vil ikke fungere med constructor innjection
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        LOG.info("Start services.");
         applicationServiceStarter.startServices();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         applicationServiceStarter.stopServices();
+        LOG.info("Stop services.");
     }
 
 }
