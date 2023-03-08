a85b76763ead3eadda16785c191d8fae921f2637
Michal J. Sladek <mrsladek@users.noreply.github.com>
2023-03-08 12:29:38 +0100

    Feat: fjerner koden som ikke brukes i klassen. (#2481)

--- integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/brevmaler/DokumentBehandlingTjeneste.java
+++ integrasjontjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/tilbakekreving/dokumentbestilling/brevmaler/DokumentBehandlingTjeneste.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.brev.BrevSporingRepository;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.BehandlingRepository;
@@ -19,14 +22,13 @@ import no.nav.foreldrepenger.tilbakekreving.dokumentbestilling.varsel.manuelt.Se
 import no.nav.foreldrepenger.tilbakekreving.dokumentbestilling.varsel.manuelt.TaskProperty;
 import no.nav.foreldrepenger.tilbakekreving.grunnlag.KravgrunnlagRepository;
 import no.nav.foreldrepenger.tilbakekreving.historikk.tjeneste.HistorikkinnslagTjeneste;
-import no.nav.foreldrepenger.tilbakekreving.selvbetjening.klient.task.SendBeskjedUtsendtVarselTilSelvbetjeningTask;
 import no.nav.vedtak.exception.TekniskException;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
-import no.nav.vedtak.felles.prosesstask.api.ProsessTaskGruppe;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 
 @ApplicationScoped
 public class DokumentBehandlingTjeneste {
+    private static final Logger LOG = LoggerFactory.getLogger(DokumentBehandlingTjeneste.class);
 
     private BehandlingRepository behandlingRepository;
     private BrevSporingRepository brevSporingRepository;
@@ -52,14 +54,13 @@ public class DokumentBehandlingTjeneste {
         this.brevSporingRepository = repositoryProvider.getBrevSporingRepository();
         this.grunnlagRepository = repositoryProvider.getGrunnlagRepository();
         this.taskTjeneste = taskTjeneste;
-
         this.historikkinnslagTjeneste = historikkinnslagTjeneste;
         this.manueltVarselBrevTjeneste = manueltVarselBrevTjeneste;
         this.innhentDokumentasjonBrevTjeneste = innhentDokumentasjonBrevTjeneste;
     }
 
     public List<BrevmalDto> hentBrevmalerFor(Long behandlingId) {
-        Behandling behandling = behandlingRepository.hentBehandling(behandlingId);
+        var behandling = behandlingRepository.hentBehandling(behandlingId);
         List<DokumentMalType> gyldigBrevMaler = new ArrayList<>();
 
         gyldigBrevMaler.add(DokumentMalType.INNHENT_DOK);
@@ -70,18 +71,18 @@ public class DokumentBehandlingTjeneste {
     }
 
     public void bestillBrev(Long behandlingId, DokumentMalType malType, String fritekst) {
-        Behandling behandling = behandlingRepository.hentBehandling(behandlingId);
+        var behandling = behandlingRepository.hentBehandling(behandlingId);
         if (DokumentMalType.VARSEL_DOK.equals(malType) || DokumentMalType.KORRIGERT_VARSEL_DOK.equals(malType)) {
             håndteresManueltSendVarsel(behandling, malType, fritekst);
         } else if (DokumentMalType.INNHENT_DOK.equals(malType)) {
-            System.out.println("Bestiller dokument type " + malType.getKode());
-            håndteresInnhentDokumentasjon(behandling, malType, fritekst);
+            LOG.info("Bestiller dokument type {}", malType.getKode());
+            håndteresInnhentDokumentasjon(behandling, fritekst);
         }
         historikkinnslagTjeneste.opprettHistorikkinnslagForBrevBestilt(behandling, malType);
     }
 
     public byte[] forhåndsvisBrev(Long behandlingId, DokumentMalType malType, String fritekst) {
-        byte[] dokument = new byte[0];
+        var dokument = new byte[0];
         if (DokumentMalType.VARSEL_DOK.equals(malType) || DokumentMalType.KORRIGERT_VARSEL_DOK.equals(malType)) {
             dokument = manueltVarselBrevTjeneste.hentForhåndsvisningManueltVarselbrev(behandlingId, malType, fritekst);
         } else if (DokumentMalType.INNHENT_DOK.equals(malType)) {
@@ -100,8 +101,8 @@ public class DokumentBehandlingTjeneste {
 
     private List<BrevmalDto> tilBrevmalDto(Behandling behandling, List<DokumentMalType> dmtList) {
         List<BrevmalDto> brevmalDtoList = new ArrayList<>(dmtList.size());
-        for (DokumentMalType dmt : dmtList) {
-            boolean tilgjengelig = sjekkOmTilgjengelig(behandling, dmt);
+        for (var dmt : dmtList) {
+            var tilgjengelig = sjekkOmTilgjengelig(behandling, dmt);
             brevmalDtoList.add(new BrevmalDto(dmt.getKode(), dmt.getNavn(), tilgjengelig));
         }
         return brevmalDtoList;
@@ -115,32 +116,27 @@ public class DokumentBehandlingTjeneste {
     }
 
     private void håndteresManueltSendVarsel(Behandling behandling, DokumentMalType malType, String fritekst) {
-        Long behandlingId = behandling.getId();
+        var behandlingId = behandling.getId();
         if (!grunnlagRepository.harGrunnlagForBehandlingId(behandlingId)) {
             throw new TekniskException("FPT-612900", String.format("Kravgrunnlag finnes ikke for behandling=%s, kan ikke sende varsel", behandlingId));
         }
 
-        ProsessTaskData sendVarselbrev = ProsessTaskData.forProsessTask(SendManueltVarselbrevTask.class);
+        var sendVarselbrev = ProsessTaskData.forProsessTask(SendManueltVarselbrevTask.class);
         sendVarselbrev.setProperty(TaskProperty.MAL_TYPE, malType.getKode());
         sendVarselbrev.setPayload(fritekst);
         sendVarselbrev.setBehandling(behandling.getFagsakId(), behandlingId, behandling.getAktørId().getId());
+        sendVarselbrev.setCallIdFraEksisterende();
 
-        ProsessTaskData sendBeskjedUtsendtVarsel = ProsessTaskData.forProsessTask(SendBeskjedUtsendtVarselTilSelvbetjeningTask.class);
-        sendBeskjedUtsendtVarsel.setBehandling(behandling.getFagsakId(), behandlingId, behandling.getAktørId().getId());
-
-        ProsessTaskGruppe taskGruppe = new ProsessTaskGruppe();
-        taskGruppe.addNesteSekvensiell(sendVarselbrev);
-        // TODO(sladek) mangler det en slik eller håndteres det nedstrøms i sendvarselbrevtask? taskGruppe.addNesteSekvensiell(sendBeskjedUtsendtVarsel);
-        taskTjeneste.lagre(taskGruppe);
+        taskTjeneste.lagre(sendVarselbrev);
     }
 
-    private void håndteresInnhentDokumentasjon(Behandling behandling, DokumentMalType malType, String fritekst) {
-        Long behandlingId = behandling.getId();
+    private void håndteresInnhentDokumentasjon(Behandling behandling, String fritekst) {
+        var behandlingId = behandling.getId();
         if (!grunnlagRepository.harGrunnlagForBehandlingId(behandlingId)) {
             throw new TekniskException("FPT-612901", String.format("Kravgrunnlag finnes ikke for behandling=%s, kan ikke sende innhent-dokumentasjonbrev", behandlingId));
         }
 
-        ProsessTaskData sendInnhentDokumentasjonBrev = ProsessTaskData.forProsessTask(InnhentDokumentasjonbrevTask.class);
+        var sendInnhentDokumentasjonBrev = ProsessTaskData.forProsessTask(InnhentDokumentasjonbrevTask.class);
         sendInnhentDokumentasjonBrev.setPayload(fritekst);
         sendInnhentDokumentasjonBrev.setBehandling(behandling.getFagsakId(), behandlingId, behandling.getAktørId().getId());
 
