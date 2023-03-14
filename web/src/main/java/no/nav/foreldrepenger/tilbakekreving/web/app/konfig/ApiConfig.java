6c36aa620f2884b56486e1d6a7397f8d69c87e09
Michal J. Sladek <mrsladek@users.noreply.github.com>
2023-03-14 22:38:08 +0100

    Fix: Match felles med bom. (#2508)
    * Fix lokal konfig.

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/konfig/ApiConfig.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/konfig/ApiConfig.java
@@ -1,17 +1,5 @@
 package no.nav.foreldrepenger.tilbakekreving.web.app.konfig;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-import org.glassfish.jersey.server.ServerProperties;
-
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import io.swagger.v3.oas.integration.GenericOpenApiContextBuilder;
 import io.swagger.v3.oas.integration.OpenApiConfigurationException;
@@ -25,12 +13,7 @@ import no.nav.foreldrepenger.tilbakekreving.web.app.exceptions.GeneralRestExcept
 import no.nav.foreldrepenger.tilbakekreving.web.app.exceptions.JsonMappingExceptionMapper;
 import no.nav.foreldrepenger.tilbakekreving.web.app.exceptions.JsonParseExceptionMapper;
 import no.nav.foreldrepenger.tilbakekreving.web.app.jackson.JacksonJsonConfig;
-import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.behandling.BehandlingFaktaRestTjeneste;
-import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.behandling.BehandlingRestTjeneste;
-import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.behandling.BrevRestTjeneste;
-import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.behandling.ForeldelseRestTjeneste;
-import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.behandling.TotrinnskontrollRestTjeneste;
-import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.behandling.VilkårsvurderingRestTjeneste;
+import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.behandling.*;
 import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.behandling.aksjonspunkt.AksjonspunktRestTjeneste;
 import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.beregningsresultat.TilbakekrevingResultatRestTjeneste;
 import no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.dokument.DokumentRestTjeneste;
@@ -51,6 +34,14 @@ import no.nav.foreldrepenger.tilbakekreving.web.server.jetty.JettyServer;
 import no.nav.vedtak.exception.TekniskException;
 import no.nav.vedtak.felles.prosesstask.rest.ProsessTaskRestTjeneste;
 
+import org.glassfish.jersey.server.ServerProperties;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 @ApplicationPath(ApiConfig.API_URI)
 public class ApiConfig extends Application {
 
@@ -61,21 +52,18 @@ public class ApiConfig extends Application {
     public ApiConfig() {
         var oas = new OpenAPI();
         var info = new Info()
-                .title("Vedtaksløsningen - Tilbakekreving")
-                .version("1.0")
-                .description("REST grensesnitt for tilbakekreving.");
+            .title("Vedtaksløsningen - Tilbakekreving")
+            .version("1.0")
+            .description("REST grensesnitt for tilbakekreving.");
 
         oas.info(info).addServersItem(new Server().url(JettyServer.getContextPath()));
 
         var oasConfig = new SwaggerConfiguration()
-                .openAPI(oas)
-                .prettyPrint(true)
-                .resourceClasses(getClasses().stream().map(Class::getName).collect(Collectors.toSet()));
+            .openAPI(oas)
+            .prettyPrint(true)
+            .resourceClasses(getClasses().stream().map(Class::getName).collect(Collectors.toSet()));
         try {
-            new GenericOpenApiContextBuilder<>()
-                    .openApiConfiguration(oasConfig)
-                    .buildContext(true)
-                    .read();
+            new GenericOpenApiContextBuilder<>().openApiConfiguration(oasConfig).buildContext(true).read();
         } catch (OpenApiConfigurationException e) {
             throw new TekniskException("OPEN-API", e.getMessage(), e);
         }
@@ -83,47 +71,44 @@ public class ApiConfig extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-        // eksponert grensesnitt
-        classes.add(ProsessTaskRestTjeneste.class);
-        classes.add(InitielleLinksRestTjeneste.class);
-        classes.add(KodeverkRestTjeneste.class);
-        classes.add(BehandlingRestTjeneste.class);
-        classes.add(AksjonspunktRestTjeneste.class);
-        classes.add(KonfigRestTjeneste.class);
-        classes.add(HistorikkRestTjeneste.class);
-        classes.add(DokumentRestTjeneste.class);
-        classes.add(ForeldelseRestTjeneste.class);
-        classes.add(VarselresponsRestTjeneste.class);
-        classes.add(BehandlingFaktaRestTjeneste.class);
-        classes.add(FeilutbetalingSisteBehandlingRestTjeneste.class);
-        classes.add(FeilutbetalingÅrsakRestTjeneste.class);
-        classes.add(VilkårsvurderingRestTjeneste.class);
-        classes.add(TilbakekrevingResultatRestTjeneste.class);
-        classes.add(TotrinnskontrollRestTjeneste.class);
-        classes.add(BrevRestTjeneste.class);
-        classes.add(FordelRestTjeneste.class);
-        classes.add(ForvaltningBehandlingRestTjeneste.class);
-        classes.add(ForvaltningKravgrunnlagRestTjeneste.class);
-        classes.add(VergeRestTjeneste.class);
-        classes.add(LosRestTjeneste.class);
+        var classes = new HashSet<>(Set.of(
+            // eksponert grensesnitt
+            ProsessTaskRestTjeneste.class,
+            InitielleLinksRestTjeneste.class,
+            KodeverkRestTjeneste.class,
+            BehandlingRestTjeneste.class,
+            AksjonspunktRestTjeneste.class,
+            KonfigRestTjeneste.class,
+            HistorikkRestTjeneste.class,
+            DokumentRestTjeneste.class,
+            ForeldelseRestTjeneste.class,
+            VarselresponsRestTjeneste.class,
+            BehandlingFaktaRestTjeneste.class,
+            FeilutbetalingSisteBehandlingRestTjeneste.class,
+            FeilutbetalingÅrsakRestTjeneste.class,
+            VilkårsvurderingRestTjeneste.class,
+            TilbakekrevingResultatRestTjeneste.class,
+            TotrinnskontrollRestTjeneste.class,
+            BrevRestTjeneste.class,
+            FordelRestTjeneste.class,
+            ForvaltningBehandlingRestTjeneste.class,
+            ForvaltningKravgrunnlagRestTjeneste.class,
+            VergeRestTjeneste.class,
+            LosRestTjeneste.class,
+            // swagger
+            OpenApiResource.class,
+            // Applikasjonsoppsett
+            JacksonJsonConfig.class,
+            // ExceptionMappers pga de som finnes i Jackson+Jersey-media
+            ConstraintViolationMapper.class,
+            JsonMappingExceptionMapper.class,
+            JsonParseExceptionMapper.class,
+            // Generell exceptionmapper m/logging for øvrige tilfelle
+            GeneralRestExceptionMapper.class));
+
         if (ENV.isLocal()) {
             classes.add(GrunnlagRestTestTjenesteLocalDev.class);
         }
-
-        // swagger
-        classes.add(OpenApiResource.class);
-
-        // Applikasjonsoppsett
-        classes.add(JacksonJsonConfig.class);
-
-        // ExceptionMappers pga de som finnes i Jackson+Jersey-media
-        classes.add(ConstraintViolationMapper.class);
-        classes.add(JsonMappingExceptionMapper.class);
-        classes.add(JsonParseExceptionMapper.class);
-
-        // Generell exceptionmapper m/logging for øvrige tilfelle
-        classes.add(GeneralRestExceptionMapper.class);
 
         return Collections.unmodifiableSet(classes);
     }
