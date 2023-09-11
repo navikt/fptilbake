2376cc2d54835901d5ba22d0ca60aeaf7bd3e30b
Anders Palfi <anders.palfi@nav.no>
2023-09-11 12:11:38 +0200

    Utvider tidspunkt for brukervarsel (#2649)

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/tjenester/fpoversikt/Sak.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/tjenester/fpoversikt/Sak.java
@@ -1,7 +1,9 @@
 package no.nav.foreldrepenger.tilbakekreving.web.app.tjenester.fpoversikt;
 
+import java.time.LocalDateTime;
+
 public record Sak(String saksnummer, Varsel varsel, boolean harVerge) {
 
-    public record Varsel(boolean sendt, boolean besvart) {
+    public record Varsel(LocalDateTime utsendtTidspunkt, boolean besvart) {
     }
 }
