1c2e90fd25fbbb296df18e2ab14c6d7dce43f23c
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-11-05 16:23:46 +0100

    TFP-5599 håndter tilfelle av åpen TBK og varselbrev

--- integrasjontjenester/ytelsesvedtak-kafka-poller/src/main/java/no/nav/foreldrepenger/tilbakekreving/hendelser/HendelseHåndtererTjeneste.java
+++ integrasjontjenester/ytelsesvedtak-kafka-poller/src/main/java/no/nav/foreldrepenger/tilbakekreving/hendelser/HendelseHåndtererTjeneste.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.tilbakekreving.hendelser;
 
+import java.time.LocalDateTime;
 import static java.time.temporal.TemporalAdjusters.next;
 
 import java.time.DayOfWeek;
@@ -13,10 +14,15 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.foreldrepenger.tilbakekreving.behandling.task.HendelseTaskDataWrapper;
 import no.nav.foreldrepenger.tilbakekreving.behandlingskontroll.impl.BehandlingskontrollTjeneste;
+import no.nav.foreldrepenger.tilbakekreving.behandlingskontroll.task.FortsettBehandlingTask;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.Behandling;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.BehandlingStegType;
+import no.nav.foreldrepenger.tilbakekreving.behandlingskontroll.impl.BehandlingskontrollTjeneste;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.BehandlingType;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.aksjonspunkt.Venteårsak;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.brev.BrevSporingRepository;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.ekstern.EksternBehandling;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.EksternBehandlingRepository;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.tilbakekrevingsvalg.VidereBehandling;
@@ -25,6 +31,8 @@ import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.FagsystemKlient;
 import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.Tillegsinformasjon;
 import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.dto.EksternBehandlingsinfoDto;
 import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.dto.TilbakekrevingValgDto;
+import no.nav.foreldrepenger.tilbakekreving.felles.Frister;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 
 @ApplicationScoped
@@ -50,7 +58,6 @@ public class HendelseHåndtererTjeneste {
                                      BehandlingRepository behandlingRepository,
                                      BehandlingskontrollTjeneste behandlingskontrollTjeneste,
                                      BrevSporingRepository brevSporingRepository) {
-
         this.taskTjeneste = taskTjeneste;
         this.fagsystemKlient = fagsystemKlient;
         this.behandlingRepository = behandlingRepository;
@@ -58,7 +65,6 @@ public class HendelseHåndtererTjeneste {
         this.brevSporingRepository = brevSporingRepository;
         this.behandlingskontrollTjeneste = behandlingskontrollTjeneste;
     }
-
 
     public void håndterHendelse(HendelseTaskDataWrapper hendelseTaskDataWrapper, Henvisning henvisning, String kaller) {
         var eksternBehandlingUuid = hendelseTaskDataWrapper.getBehandlingUuid();
@@ -75,31 +81,41 @@ public class HendelseHåndtererTjeneste {
             behandlingskontrollTjeneste.settBehandlingPåVentUtenSteg(åpenTilbakekreving, AksjonspunktDefinisjon.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG,
                 venteTid.atStartOfDay(), Venteårsak.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG);
         }
+        var harSendtVarselTidligere = åpenTilbakekreving != null && brevSporingRepository.harVarselBrevSendtForBehandlingId(åpenTilbakekreving.getId());
         if (erRelevantHendelseForOpprettTilbakekreving(tbkData)) {
-            if (åpenTilbakekreving != null) {
-                // TODO TFP-5599 - burde vi satt evt åpen TBK på vent? Eller er det så kort tid før det kommer noe fra OS at det ikke er et problem?
-                if  (samletBehandlingInfo.getVarseltekst() == null || samletBehandlingInfo.getVarseltekst().isBlank()) {
-                    // Do nothing. Det skal ikke sendes varsel å spiller liten rolle om det er sendt tidligere varsel eller ikke
-                    return;
-                } else {
-                    var harSendtVarselTidligere = brevSporingRepository.harVarselBrevSendtForBehandlingId(åpenTilbakekreving.getId()) ?
-                        "er varslet tidligere" : "ikke er varslet";
-                    LOG.info("Mottatt VedtakHendelse {} har bedt om varsel og det finnes åpen tilbakekreving som {}", tbkData.getVidereBehandling(), harSendtVarselTidligere);
-                }
-            }
-            // TODO TFP-5599 vurdere else-logikk her
             if (eksternBehandlingRepository.harEksternBehandlingForEksternUuid(eksternBehandlingUuid)) {
                 LOG.info("Mottatt VedtakHendelse {} allerede opprettet tilbakekreving for henvisning={} fra {}", tbkData.getVidereBehandling(), henvisning, kaller);
+            }
+            // Håndter åpen TBK eller opprett ny.
+            if (åpenTilbakekreving != null) {
+                // Enten vent på nytt grunnlag / ny status - eller start på nytt med nytt varselbrev
+                if  (samletBehandlingInfo.getVarseltekst() == null || samletBehandlingInfo.getVarseltekst().isBlank()) {
+                    // Det skal ikke sendes varsel og spiller liten rolle om det er sendt tidligere varsel eller ikke.
+                    // TODO TFP-5599 riktig å sette på vent?
+                    behandlingskontrollTjeneste.settBehandlingPåVentUtenSteg(åpenTilbakekreving, AksjonspunktDefinisjon.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG,
+                        LocalDateTime.now().plus(Frister.KRAVGRUNNLAG_FØRSTE), Venteårsak.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG);
+                } else {
+                    var loggVarsel = harSendtVarselTidligere ? "er varslet tidligere" : "ikke er varslet";
+                    LOG.info("Mottatt VedtakHendelse {} har bedt om varsel og det finnes åpen tilbakekreving som {}", tbkData.getVidereBehandling(), loggVarsel);
+                    // Brute-force rewind slik at det sendes nytt varsel (og man venter på grunnlag)
+                    rewindTilbakekrevingTilStart(åpenTilbakekreving, henvisning, eksternBehandlingUuid);
+                }
             } else {
-                LOG.info("Mottatt VedtakHendelse {} er relevant for tilbakekreving opprett for henvisning={} fra {}", tbkData.getVidereBehandling(), henvisning, kaller);
+                LOG.info("Mottatt VedtakHendelse {} er relevant for tilbakekreving opprett for henvisning={} fra {}", tbkData.getVidereBehandling(),
+                    henvisning, kaller);
                 lagOpprettBehandlingTask(hendelseTaskDataWrapper, henvisning);
             }
         } else if (erRelevantHendelseForOppdatereTilbakekreving(tbkData)) {
-            // TODO TFP-5599 - burde denne satt evt åpen TBK på vent? Eller er det så kort tid før det kommer noe fra OS at det ikke er et problem?
-            LOG.info("Mottatt VedtakHendelse {} for henvisning={} var tidligere relevant for å oppdatere behandling. Nå ignoreres den",
+            LOG.info("Mottatt VedtakHendelse {} for henvisning {} var tidligere relevant for å oppdatere behandling. Nå ignoreres den",
                 tbkData.getVidereBehandling(), henvisning);
+            // TODO TFP-5599 riktig å sette på vent?
+            if (åpenTilbakekreving != null) {
+                behandlingskontrollTjeneste.settBehandlingPåVentUtenSteg(åpenTilbakekreving, AksjonspunktDefinisjon.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG,
+                    LocalDateTime.now().plus(Frister.KRAVGRUNNLAG_FØRSTE), Venteårsak.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG);
+            }
         }
     }
+
 
     public Henvisning hentHenvisning(UUID behandling) {
         return fagsystemKlient.hentBehandlingOptional(behandling)
@@ -125,5 +141,21 @@ public class HendelseHåndtererTjeneste {
         taskData.setBehandlingType(BehandlingType.TILBAKEKREVING);
 
         taskTjeneste.lagre(taskData.getProsessTaskData());
+    }
+
+    private void rewindTilbakekrevingTilStart(Behandling åpenTilbakekreving, Henvisning henvisning, UUID eksternBehandlingUuid) {
+        EksternBehandling eksternBehandling = new EksternBehandling(åpenTilbakekreving, henvisning, eksternBehandlingUuid);
+        eksternBehandlingRepository.lagre(eksternBehandling);
+        var kontekst = behandlingskontrollTjeneste.initBehandlingskontroll(åpenTilbakekreving);
+        behandlingskontrollTjeneste.taBehandlingAvVentSetAlleAutopunktUtført(åpenTilbakekreving, kontekst);
+        behandlingskontrollTjeneste.behandlingTilbakeføringTilTidligereBehandlingSteg(kontekst, BehandlingStegType.INOPPSTEG);
+        lagFortsettBehandlingTask(åpenTilbakekreving);
+    }
+
+    private void lagFortsettBehandlingTask(Behandling behandling) {
+        ProsessTaskData taskData = ProsessTaskData.forProsessTask(FortsettBehandlingTask.class);
+        taskData.setBehandling(behandling.getFagsakId(), behandling.getId(), behandling.getAktørId().getId());
+        taskData.setCallIdFraEksisterende();
+        taskTjeneste.lagre(taskData);
     }
 }
