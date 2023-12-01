759c775d26f5f30a36bdefcdcca73a9c68b2c2bc
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-12-01 11:26:21 +0100

    Sett åpen tbk på vent ved nytt vedtak

--- integrasjontjenester/ytelsesvedtak-kafka-poller/src/main/java/no/nav/foreldrepenger/tilbakekreving/hendelser/HendelseHåndtererTjeneste.java
+++ integrasjontjenester/ytelsesvedtak-kafka-poller/src/main/java/no/nav/foreldrepenger/tilbakekreving/hendelser/HendelseHåndtererTjeneste.java
@@ -1,9 +1,19 @@
 package no.nav.foreldrepenger.tilbakekreving.hendelser;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+
+import no.nav.foreldrepenger.tilbakekreving.behandlingskontroll.impl.BehandlingskontrollTjeneste;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.aksjonspunkt.Venteårsak;
+import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.BehandlingRepository;
+
+import no.nav.foreldrepenger.tilbakekreving.felles.Frister;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +28,8 @@ import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.dto.EksternBehandli
 import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.dto.TilbakekrevingValgDto;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 
+import static java.time.temporal.TemporalAdjusters.next;
+
 @ApplicationScoped
 public class HendelseHåndtererTjeneste {
 
@@ -26,6 +38,8 @@ public class HendelseHåndtererTjeneste {
     private ProsessTaskTjeneste taskTjeneste;
     private FagsystemKlient fagsystemKlient;
     private EksternBehandlingRepository eksternBehandlingRepository;
+    private BehandlingRepository behandlingRepository;
+    private BehandlingskontrollTjeneste behandlingskontrollTjeneste;
 
     HendelseHåndtererTjeneste() {
         // CDI
@@ -34,18 +48,28 @@ public class HendelseHåndtererTjeneste {
     @Inject
     public HendelseHåndtererTjeneste(ProsessTaskTjeneste taskTjeneste,
                                      FagsystemKlient fagsystemKlient,
-                                     EksternBehandlingRepository eksternBehandlingRepository) {
+                                     EksternBehandlingRepository eksternBehandlingRepository,
+                                     BehandlingRepository behandlingRepository, BehandlingskontrollTjeneste behandlingskontrollTjeneste) {
         this.taskTjeneste = taskTjeneste;
         this.fagsystemKlient = fagsystemKlient;
         this.eksternBehandlingRepository = eksternBehandlingRepository;
-    }
-
-    public void håndterHendelse(HendelseTaskDataWrapper hendelseTaskDataWrapper, Henvisning henvisning) {
-        håndterHendelse(hendelseTaskDataWrapper, henvisning, "test");
+        this.behandlingRepository = behandlingRepository;
+        this.behandlingskontrollTjeneste = behandlingskontrollTjeneste;
     }
 
     public void håndterHendelse(HendelseTaskDataWrapper hendelseTaskDataWrapper, Henvisning henvisning, String kaller) {
         var eksternBehandlingUuid = hendelseTaskDataWrapper.getBehandlingUuid();
+        var åpenTilbakekreving = behandlingRepository.finnÅpenTilbakekrevingsbehandling(hendelseTaskDataWrapper.getSaksnummer())
+            .orElse(null);
+        if (åpenTilbakekreving != null) {
+            // For å redusere risiko for at det fattes vedtak basert på gammelt kravgrunnlag
+            // Nytt ytelsesvedtak når det finnes åpen tilbakekreving vil ofte føre til at kravgrunnlag sperres samme kveld
+            // Gjenoppta-batch kjører hverdager kl 07:00. Hvis helg, vent til tirsdag morgen, ellers 24 timer.
+            var venteTid = DayOfWeek.FRIDAY.getValue() < DayOfWeek.from(LocalDate.now()).getValue() ?
+                LocalDate.now().with(next(DayOfWeek.TUESDAY)) : LocalDate.now().plusDays(1);
+            behandlingskontrollTjeneste.settBehandlingPåVentUtenSteg(åpenTilbakekreving, AksjonspunktDefinisjon.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG,
+                venteTid.atStartOfDay(), Venteårsak.VENT_PÅ_TILBAKEKREVINGSGRUNNLAG);
+        }
         fagsystemKlient.hentTilbakekrevingValg(eksternBehandlingUuid)
                 .ifPresent(tbkData -> {
                     if (erRelevantHendelseForOpprettTilbakekreving(tbkData)) {
