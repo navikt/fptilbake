853086e3204d1ac70186e783fb4d32a358957f0c
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-03-21 21:25:51 +0100

    Nytt KConsumer pattern (#2782)

--- integrasjontjenester/ytelsesvedtak-kafka-poller/src/main/java/no/nav/foreldrepenger/tilbakekreving/hendelser/VedtakConsumer.java
+++ integrasjontjenester/ytelsesvedtak-kafka-poller/src/main/java/no/nav/foreldrepenger/tilbakekreving/hendelser/VedtakConsumer.java
@@ -1,90 +1,49 @@
 package no.nav.foreldrepenger.tilbakekreving.hendelser;
 
-import static org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.SHUTDOWN_CLIENT;
+import java.util.List;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.foreldrepenger.konfig.KonfigVerdi;
-import no.nav.foreldrepenger.tilbakekreving.fagsystem.ApplicationName;
-import no.nav.vedtak.felles.integrasjon.kafka.KafkaProperties;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import no.nav.vedtak.felles.integrasjon.kafka.KafkaConsumerManager;
 import no.nav.vedtak.log.metrics.Controllable;
 import no.nav.vedtak.log.metrics.LiveAndReadinessAware;
 
 @ApplicationScoped
-public class VedtakConsumer  {
+public class VedtakConsumer implements LiveAndReadinessAware, Controllable {
 
     private static final Logger LOG = LoggerFactory.getLogger(VedtakConsumer.class);
-    private KafkaStreams stream;
-    private String topic;
+    private KafkaConsumerManager<String, String> kcm;
 
     VedtakConsumer() {
     }
 
     @Inject
-    public VedtakConsumer(@KonfigVerdi(value = "kafka.fattevedtak.topic", defaultVerdi = "teamforeldrepenger.familie-vedtakfattet-v1") String topicName,
-                          VedtaksHendelseHåndterer vedtaksHendelseHåndterer) {
-        this.topic = topicName;
-        var applicationId = ApplicationName.hvilkenTilbakeAppName() + "-vedtak"; // Hold denne konstant pga offset-commit
-
-        final Consumed<String, String> consumed = Consumed.with(Topology.AutoOffsetReset.EARLIEST);
-
-        final var builder = new StreamsBuilder();
-        builder.stream(topic, consumed)
-            .foreach(vedtaksHendelseHåndterer::handleMessage);
-
-        this.stream = new KafkaStreams(builder.build(), KafkaProperties.forStreamsStringValue(applicationId));
+    public VedtakConsumer(VedtaksHendelseHåndterer vedtaksHendelseHåndterer) {
+        this.kcm = new KafkaConsumerManager<>(List.of(vedtaksHendelseHåndterer));
     }
 
-    private void addShutdownHooks() {
-        stream.setStateListener((newState, oldState) -> {
-            LOG.info("{} :: From state={} to state={}", topic, oldState, newState);
-
-            if (newState == KafkaStreams.State.ERROR) {
-                // if the stream has died there is no reason to keep spinning
-                LOG.warn("{} :: No reason to keep living, closing stream", topic);
-                stop();
-            }
-        });
-        stream.setUncaughtExceptionHandler(ex -> {
-            LOG.error(topic + " :: Caught exception in stream, exiting", ex);
-            return SHUTDOWN_CLIENT;
-        });
-    }
-
-
+    @Override
     public boolean isAlive() {
-        return stream != null && stream.state().isRunningOrRebalancing();
+        return kcm.allRunning();
     }
 
+    @Override
     public boolean isReady() {
         return isAlive();
     }
 
-    public String getTopic() {
-        return topic;
-    }
-
+    @Override
     public void start() {
-        addShutdownHooks();
-        stream.start();
-        LOG.info("Starter konsumering av topic={}, tilstand={}", topic, stream.state());
+        LOG.info("Starter konsumering av topics={}", kcm.topicNames());
+        kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
     }
 
+    @Override
     public void stop() {
-        var timeoutSekunder = 30;
-        LOG.info("Starter shutdown av topic={}, tilstand={} med {} sekunder timeout", topic, stream.state(), timeoutSekunder);
-        stream.close(Duration.of(timeoutSekunder, ChronoUnit.SECONDS));
-        LOG.info("Shutdown av topic={}, tilstand={} med {} sekunder timeout", topic, stream.state(), timeoutSekunder);
+        LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
+        kcm.stop();
     }
 }
