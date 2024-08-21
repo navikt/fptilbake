ae59508086a47a592b71ec15cf7c656cf5643c51
Audun Eltvik <audun.eltvik@nav.no>
2024-08-21 15:49:28 +0200

    Bruk riktig kolonnenavn

--- migreringer/src/main/resources/db/migration/defaultDS/1.3/V1.3_06__nytt_fritekstfelt_i_varsel.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/1.3/V1.3_06__nytt_fritekstfelt_i_varsel.sql
@@ -1,3 +1,3 @@
 alter table VARSEL add VARSEL_FRITEKST_UTVIDET CLOB;
 comment on column VARSEL.VARSEL_FRITEKST_UTVIDET is 'fritekst som brukes i varselbrev, med plass til flere tegn';
-update VARSEL set VARSEL_FRITEKST_UTVIDET = FRITEKST_VARSEL;
+update VARSEL set VARSEL_FRITEKST_UTVIDET = VARSEL_FRITEKST;
