8ae498443dd501f471e6cb0f80fe058a2c7c5816
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-08-23 20:40:41 +0200

    FPfelles tilpasset k9tilbake (#2856)
    * FPfelles tilpasset k9tilbake
    
    * Fikse test

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/konfig/AuthenticationFilter.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/konfig/AuthenticationFilter.java
@@ -8,13 +8,19 @@ import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.container.ResourceInfo;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Cookie;
 import jakarta.ws.rs.ext.Provider;
 import no.nav.foreldrepenger.tilbakekreving.web.server.jetty.JettyServer;
 import no.nav.vedtak.sikkerhet.jaxrs.AuthenticationFilterDelegate;
+import no.nav.vedtak.sikkerhet.oidc.token.TokenString;
+
+import java.util.Optional;
 
 @Provider
 @Priority(Priorities.AUTHENTICATION)
 public class AuthenticationFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final String K9_ID_TOKEN_COOKIE_NAME = "ID_token";
 
     @Context
     private ResourceInfo resourceinfo;
@@ -30,7 +36,20 @@ public class AuthenticationFilter implements ContainerRequestFilter, ContainerRe
 
     @Override
     public void filter(ContainerRequestContext req) {
-        AuthenticationFilterDelegate.validerSettKontekst(resourceinfo, req, JettyServer.getCookiePath()); // Særordning for K9-tilbake
+        AuthenticationFilterDelegate.validerSettKontekst(resourceinfo, req, () -> getToken(req)); // Særordning for K9-tilbake
+    }
+
+    private static Optional<TokenString> getToken(ContainerRequestContext request) {
+        return AuthenticationFilterDelegate.getTokenFromHeader(request).or(() -> getCookieToken(request));
+    }
+
+    private static Optional<TokenString> getCookieToken(ContainerRequestContext request) {
+        var cookiePath = JettyServer.getCookiePath();
+        var idTokenCookie = Optional.ofNullable(request.getCookies()).map(c -> c.get(K9_ID_TOKEN_COOKIE_NAME));
+        return idTokenCookie.filter(c -> cookiePath != null && cookiePath.equalsIgnoreCase(c.getPath()))
+            .or(() -> idTokenCookie)
+            .map(Cookie::getValue)
+            .map(TokenString::new);
     }
 
 }
