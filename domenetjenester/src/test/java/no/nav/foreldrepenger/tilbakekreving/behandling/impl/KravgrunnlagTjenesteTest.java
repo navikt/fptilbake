6a40885f376b4ec5792bbc0d1569c0c8be4a7cbc
Michal J. Sladek <michal.sladek@nav.no>
2024-11-29 14:12:36 +0100

    Test: fix

--- domenetjenester/src/test/java/no/nav/foreldrepenger/tilbakekreving/behandling/impl/KravgrunnlagTjenesteTest.java
+++ domenetjenester/src/test/java/no/nav/foreldrepenger/tilbakekreving/behandling/impl/KravgrunnlagTjenesteTest.java
@@ -59,7 +59,7 @@ class KravgrunnlagTjenesteTest extends FellesTestOppsett {
         entityManager.setFlushMode(FlushModeType.AUTO);
         when(mockTpsTjeneste.hentAktørForFnr(new PersonIdent(SSN))).thenReturn(Optional.of(aktørId));
         kravgrunnlagTjeneste = new KravgrunnlagTjeneste(repoProvider, mockGjenopptaBehandlingTjeneste, behandlingskontrollTjeneste, eventPubliserer,
-            new AutomatiskSaksbehandlingVurderingTjeneste(grunnlagRepository, varselRepository), entityManager);
+            new AutomatiskSaksbehandlingVurderingTjeneste(grunnlagRepository, varselRepository));
     }
 
     @Test
