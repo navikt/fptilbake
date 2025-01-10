a6d0a1375822753ddff6b5af985d66b8592a3740
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-01-10 13:27:07 +0100

    Støtte for henvisning-format som brukes for ungdomsytelsen (#2912)

--- integrasjontjenester/k9-sak-klient/src/main/java/no/nav/foreldrepenger/tilbakekreving/k9sak/klient/K9HenvisningKonverterer.java
+++ integrasjontjenester/k9-sak-klient/src/main/java/no/nav/foreldrepenger/tilbakekreving/k9sak/klient/K9HenvisningKonverterer.java
@@ -9,18 +9,28 @@ import no.nav.foreldrepenger.tilbakekreving.domene.typer.Henvisning;
 public class K9HenvisningKonverterer {
 
     public static Henvisning uuidTilHenvisning(UUID uuid) {
+        return uuidTilHenvisning(uuid, null);
+    }
+
+    public static Henvisning uuidTilHenvisning(UUID uuid, Integer sekvensnummer) {
         ByteBuffer buffer = ByteBuffer.allocate(2 * Long.BYTES);
         buffer.putLong(uuid.getMostSignificantBits());
         buffer.putLong(uuid.getLeastSignificantBits());
         String base64encodedWithPadding = Base64.getEncoder().encodeToString(buffer.array());
         int paddingStart = base64encodedWithPadding.indexOf("=");
         String henvisningString = paddingStart == -1 ? base64encodedWithPadding : base64encodedWithPadding.substring(0, paddingStart);
+        if (sekvensnummer != null) {
+            henvisningString += "=" + sekvensnummer;
+        }
         return new Henvisning(henvisningString);
     }
 
     public static UUID henvisningTilUuid(Henvisning henvisning) {
+        String henvisningStreng = henvisning.getVerdi();
+        String henvisningUuidDel = henvisningStreng.contains("=") ? henvisningStreng.split("=")[0] : henvisningStreng;
+
         ByteBuffer buffer = ByteBuffer.allocate(2 * Long.BYTES);
-        buffer.put(Base64.getDecoder().decode(henvisning.getVerdi()));
+        buffer.put(Base64.getDecoder().decode(henvisningUuidDel));
         buffer.flip();
         long mostSignificantBits = buffer.getLong();
         long leastSignificantBits = buffer.getLong();
