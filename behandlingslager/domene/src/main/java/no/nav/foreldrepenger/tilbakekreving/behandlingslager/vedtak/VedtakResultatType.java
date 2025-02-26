4f7b715edc723efb70af065db871bee82ce12b27
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-02-26 22:02:08 +0100

    VedtakResultatType => JsonValue

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/vedtak/VedtakResultatType.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/vedtak/VedtakResultatType.java
@@ -1,7 +1,5 @@
 package no.nav.foreldrepenger.tilbakekreving.behandlingslager.vedtak;
 
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -9,16 +7,10 @@ import java.util.Map;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.kodeverk.Kodeverdi;
-import no.nav.foreldrepenger.tilbakekreving.behandlingslager.kodeverk.TempAvledeKode;
 
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
-@JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
 public enum VedtakResultatType implements Kodeverdi { //Kun brukes for å sende data til frontend
 
     FULL_TILBAKEBETALING("FULL_TILBAKEBETALING", "Tilbakebetaling"),
@@ -44,12 +36,10 @@ public enum VedtakResultatType implements Kodeverdi { //Kun brukes for å sende 
         this.navn = navn;
     }
 
-    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-    public static VedtakResultatType fraKode(@JsonProperty(value = "kode") Object node) {
-        if (node == null) {
+    public static VedtakResultatType fraKode(final String kode) {
+        if (kode == null) {
             return null;
         }
-        String kode = TempAvledeKode.getVerdi(VedtakResultatType.class, node, "kode");
         var ad = KODER.get(kode);
         if (ad == null) {
             throw new IllegalArgumentException("Ukjent VedtakResultatType: " + kode);
@@ -61,19 +51,17 @@ public enum VedtakResultatType implements Kodeverdi { //Kun brukes for å sende 
         return Collections.unmodifiableMap(KODER);
     }
 
-    @JsonProperty
+    @JsonValue
     @Override
     public String getKode() {
         return kode;
     }
 
-    @JsonProperty
     @Override
     public String getKodeverk() {
         return KODEVERK;
     }
 
-    @JsonProperty
     @Override
     public String getNavn() {
         return navn;
