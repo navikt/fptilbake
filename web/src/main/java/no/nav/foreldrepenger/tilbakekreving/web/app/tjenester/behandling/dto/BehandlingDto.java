513c03e9730db56e98145b58b54bf5ecfe1e3cb2
Siri Mykland <siri.mykland@nav.no>
2025-03-19 14:49:10 +0100

    fjern ubrukt import i BehandlingDto

--- web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/tjenester/behandling/dto/BehandlingDto.java
+++ web/src/main/java/no/nav/foreldrepenger/tilbakekreving/web/app/tjenester/behandling/dto/BehandlingDto.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.BehandlingStatus;
