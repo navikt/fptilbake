1182f77126748b51f2da822d87836feb210f3906
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-09-23 12:41:53 +0200

    TFP-6395 bruk nye endepunkt mot fpsak

--- integrasjontjenester/fpsak-klient/src/main/java/no/nav/foreldrepenger/tilbakekreving/fpsak/klient/dto/FpsakBehandlingInfoDto.java
+++ integrasjontjenester/fpsak-klient/src/main/java/no/nav/foreldrepenger/tilbakekreving/fpsak/klient/dto/FpsakBehandlingInfoDto.java
@@ -1,32 +1,28 @@
 package no.nav.foreldrepenger.tilbakekreving.fpsak.klient.dto;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import no.nav.foreldrepenger.tilbakekreving.domene.typer.Henvisning;
 import no.nav.foreldrepenger.tilbakekreving.fagsystem.klient.dto.EksternBehandlingsinfoDto;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FpsakBehandlingInfoDto extends EksternBehandlingsinfoDto {
 
-    private Long id;
-    private List<BehandlingResourceLinkDto> links = new ArrayList<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<BehandlingResourceLinkDto> getLinks() {
-        return links;
-    }
-
-    public void setLinks(List<BehandlingResourceLinkDto> links) {
-        this.links = links;
+    public static FpsakBehandlingInfoDto fraFullDto(FpsakTilbakeDto dto) {
+        var info = new FpsakBehandlingInfoDto();
+        info.setHenvisning(Henvisning.fraEksternBehandlingId(dto.behandling().henvisning().henvisning()));
+        info.setUuid(dto.behandling().uuid());
+        info.setBehandlendeEnhetId(dto.behandling().behandlendeEnhetId());
+        info.setBehandlendeEnhetNavn(dto.behandling().behandlendeEnhetNavn());
+        info.setVedtakDato(dto.behandling().vedtaksdato());
+        info.setSprakkode(switch (dto.behandling().språkkode()) {
+            case NB -> no.nav.foreldrepenger.tilbakekreving.behandlingslager.geografisk.Språkkode.NB;
+            case NN -> no.nav.foreldrepenger.tilbakekreving.behandlingslager.geografisk.Språkkode.NN;
+            case EN -> no.nav.foreldrepenger.tilbakekreving.behandlingslager.geografisk.Språkkode.EN;
+            case null -> no.nav.foreldrepenger.tilbakekreving.behandlingslager.geografisk.Språkkode.NB;
+        });
+        return info;
     }
 
 }
