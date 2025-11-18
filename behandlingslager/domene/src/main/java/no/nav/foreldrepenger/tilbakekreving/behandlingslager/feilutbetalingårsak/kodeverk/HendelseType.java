2fa37a9ca5e51a4209961c90813ef4cb19d9cfa8
Siri Mykland <siri.mykland@nav.no>
2025-11-18 15:55:37 +0100

    standariser annotering av lovreferanser

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/feilutbetalingårsak/kodeverk/HendelseType.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/feilutbetalingårsak/kodeverk/HendelseType.java
@@ -13,35 +13,35 @@ import no.nav.foreldrepenger.tilbakekreving.behandlingslager.kodeverk.Kodeverdi;
 
 public enum HendelseType implements Kodeverdi {
 
-    MEDLEMSKAP_TYPE("MEDLEMSKAP", "§14-2 Medlemskap", 10),
+    MEDLEMSKAP_TYPE("MEDLEMSKAP", "§ 14-2 Medlemskap", 10),
     ØKONOMI_FEIL("OKONOMI_FEIL", "Feil i økonomi", 500),
-    FP_OPPTJENING_TYPE("OPPTJENING_TYPE", "§14-6 Opptjening", 20),
-    FP_BEREGNING_TYPE("BEREGNING_TYPE", "§14-7 Beregning", 30),
-    FP_STONADSPERIODEN_TYPE("STONADSPERIODEN_TYPE", "§14-9 Stønadsperioden", 40),
-    FP_UTTAK_GENERELT_TYPE("UTTAK_GENERELT_TYPE", "§14-10 Generelt om uttak", 50),
-    FP_UTTAK_UTSETTELSE_TYPE("UTTAK_UTSETTELSE_TYPE", "§14-11 Utsettelse av uttak", 60),
-    FP_UTTAK_KVOTENE_TYPE("UTTAK_KVOTENE_TYPE", "§14-12 Uttak av kvotene", 70),
-    FP_VILKAAR_GENERELLE_TYPE("VILKAAR_GENERELLE_TYPE", "§14-13 Generelle vilkår for fars uttak", 80),
-    FP_KUN_RETT_TYPE("KUN_RETT_TYPE", "§14-14 Kun far/medmor rett", 90),
-    FP_UTTAK_ALENEOMSORG_TYPE("UTTAK_ALENEOMSORG_TYPE", "§14-15 Uttak ved aleneomsorg/samlivsbrudd", 100),//NOSONAR
-    FP_UTTAK_GRADERT_TYPE("UTTAK_GRADERT_TYPE", "§14-16 Gradert uttak", 110),
+    FP_OPPTJENING_TYPE("OPPTJENING_TYPE", "§ 14-6 Opptjening", 20),
+    FP_BEREGNING_TYPE("BEREGNING_TYPE", "§ 14-7 Beregning", 30),
+    FP_STONADSPERIODEN_TYPE("STONADSPERIODEN_TYPE", "§ 14-9 Stønadsperioden", 40),
+    FP_UTTAK_GENERELT_TYPE("UTTAK_GENERELT_TYPE", "§ 14-10 Generelt om uttak", 50),
+    FP_UTTAK_UTSETTELSE_TYPE("UTTAK_UTSETTELSE_TYPE", "§ 14-11 Utsettelse av uttak", 60),
+    FP_UTTAK_KVOTENE_TYPE("UTTAK_KVOTENE_TYPE", "§ 14-12 Uttak av kvotene", 70),
+    FP_VILKAAR_GENERELLE_TYPE("VILKAAR_GENERELLE_TYPE", "§ 14-13 Generelle vilkår for fars uttak", 80),
+    FP_KUN_RETT_TYPE("KUN_RETT_TYPE", "§ 14-14 Kun far/medmor rett", 90),
+    FP_UTTAK_ALENEOMSORG_TYPE("UTTAK_ALENEOMSORG_TYPE", "§ 14-15 Uttak ved aleneomsorg/samlivsbrudd", 100),//NOSONAR
+    FP_UTTAK_GRADERT_TYPE("UTTAK_GRADERT_TYPE", "§ 14-16 Gradert uttak", 110),
     FP_ANNET_HENDELSE_TYPE("FP_ANNET_HENDELSE_TYPE", "Annet", 999),//NOSONAR
 
-    ES_MEDLEMSKAP_TYPE("ES_MEDLEMSKAP", "§14-2 Medlemskap", 10),
-    ES_FODSELSVILKAARET_TYPE("ES_FODSELSVILKAARET_TYPE", "§14-17 1. ledd Fødselsvilkåret", 30),
-    ES_ADOPSJONSVILKAARET_TYPE("ES_ADOPSJONSVILKAARET_TYPE", "§14-17 1. ledd Adopsjonsvilkåret", 20),
-    ES_FORELDREANSVAR_TYPE("ES_FORELDREANSVAR_TYPE", "§14-17 2. ledd Foreldreansvar ", 40),
-    ES_OMSORGSVILKAAR_TYPE("ES_OMSORGSVILKAAR_TYPE", "§14-17 3. ledd Omsorgsvilkår ved mors død", 50),
-    ES_FORELDREANSVAR_FAR_TYPE("ES_FORELDREANSVAR_FAR_TYPE", "§14-17 4. ledd Foreldreansvar far", 60),
+    ES_MEDLEMSKAP_TYPE("ES_MEDLEMSKAP", "§ 14-2 Medlemskap", 10),
+    ES_FODSELSVILKAARET_TYPE("ES_FODSELSVILKAARET_TYPE", "§ 14-17 første ledd Fødselsvilkåret", 30),
+    ES_ADOPSJONSVILKAARET_TYPE("ES_ADOPSJONSVILKAARET_TYPE", "§ 14-17 første ledd Adopsjonsvilkåret", 20),
+    ES_FORELDREANSVAR_TYPE("ES_FORELDREANSVAR_TYPE", "§ 14-17 andre ledd Foreldreansvar ", 40),
+    ES_OMSORGSVILKAAR_TYPE("ES_OMSORGSVILKAAR_TYPE", "§ 14-17 tredje ledd Omsorgsvilkår ved mors død", 50),
+    ES_FORELDREANSVAR_FAR_TYPE("ES_FORELDREANSVAR_FAR_TYPE", "§ 14-17 fjerde ledd Foreldreansvar far", 60),
     ES_RETT_PAA_FORELDREPENGER_TYPE("ES_RETT_PAA_FORELDREPENGER_TYPE", "Rett på foreldrepenger etter klage", 70),
     ES_FEIL_UTBETALING_TYPE("ES_FEIL_UTBETALING_TYPE", "Feil i utbetaling", 500),
     ES_ANNET_TYPE("ES_ANNET_TYPE", "Annet", 999),//NOSONAR
 
-    SVP_FAKTA_TYPE("SVP_FAKTA_TYPE", "§14-4 Fakta om svangerskap", 20),
-    SVP_ARBEIDSGIVERS_FORHOLD_TYPE("SVP_ARBEIDSGIVERS_FORHOLD_TYPE", "§14-4 1. ledd Arbeidsgivers forhold", 30),
-    SVP_OPPTJENING_TYPE("SVP_OPPTJENING_TYPE", "§14-4 3. ledd Opptjening/ inntekt", 50),
-    SVP_BEREGNING_TYPE("SVP_BEREGNING_TYPE", "§14-4 5. ledd Beregning ", 60),
-    SVP_UTTAK_TYPE("SVP_UTTAK_TYPE", "§14-4 Uttak", 70),
+    SVP_FAKTA_TYPE("SVP_FAKTA_TYPE", "§ 14-4 Fakta om svangerskap", 20),
+    SVP_ARBEIDSGIVERS_FORHOLD_TYPE("SVP_ARBEIDSGIVERS_FORHOLD_TYPE", "§ 14-4 første ledd Arbeidsgivers forhold", 30),
+    SVP_OPPTJENING_TYPE("SVP_OPPTJENING_TYPE", "§ 14-4 3. ledd Opptjening/ inntekt", 50),
+    SVP_BEREGNING_TYPE("SVP_BEREGNING_TYPE", "§ 14-4 5. ledd Beregning ", 60),
+    SVP_UTTAK_TYPE("SVP_UTTAK_TYPE", "§ 14-4 Uttak", 70),
     SVP_OPPHØR("OPPHØR", "Opphør", 80),
     SVP_ANNET_TYPE("SVP_ANNET_TYPE", "Annet", 999),//NOSONAR
 
--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/vilkår/kodeverk/VilkårResultat.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandlingslager/vilkår/kodeverk/VilkårResultat.java
@@ -13,10 +13,10 @@ import no.nav.foreldrepenger.tilbakekreving.behandlingslager.kodeverk.Kodeverdi;
 
 public enum VilkårResultat implements Kodeverdi {
 
-    FORSTO_BURDE_FORSTÅTT("FORSTO_BURDE_FORSTAATT", "Ja, mottaker forsto eller burde forstått at utbetalingen skyldtes en feil (1. ledd, 1. punkt)"),
-    MANGELFULLE_OPPLYSNINGER_FRA_BRUKER("MANGELFULL_OPPLYSNING", "Ja, mottaker har forårsaket feilutbetalingen ved forsett eller uaktsomt gitt mangelfulle opplysninger (1. ledd, 2 punkt)"),
-    FEIL_OPPLYSNINGER_FRA_BRUKER("FEIL_OPPLYSNINGER", "Ja, mottaker har forårsaket feilutbetalingen ved forsett eller uaktsomt gitt feilaktige opplysninger (1. ledd, 2 punkt)"),
-    GOD_TRO("GOD_TRO", "Nei, mottaker har mottatt beløpet i god tro (1. ledd)"),
+    FORSTO_BURDE_FORSTÅTT("FORSTO_BURDE_FORSTAATT", "Ja, mottaker forsto eller burde forstått at utbetalingen skyldtes en feil (første ledd, første punkt)"),
+    MANGELFULLE_OPPLYSNINGER_FRA_BRUKER("MANGELFULL_OPPLYSNING", "Ja, mottaker har forårsaket feilutbetalingen ved forsett eller uaktsomt gitt mangelfulle opplysninger (første ledd, andre punkt)"),
+    FEIL_OPPLYSNINGER_FRA_BRUKER("FEIL_OPPLYSNINGER", "Ja, mottaker har forårsaket feilutbetalingen ved forsett eller uaktsomt gitt feilaktige opplysninger (første ledd, andre punkt)"),
+    GOD_TRO("GOD_TRO", "Nei, mottaker har mottatt beløpet i god tro (første ledd)"),
     UDEFINERT("-", "Ikke Definert");
 
     private String kode;
