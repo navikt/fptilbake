600bf7dbe63c987198e33f74bc019276f46d7646
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-05-20 21:23:43 +0200

    TFP-6978 forenkle vedtakskvittering. Bare lagre ved positivt svar, feil ellers

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandling/steg/iverksettvedtak/SendVedtakTilOppdragsystemetTask.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/tilbakekreving/behandling/steg/iverksettvedtak/SendVedtakTilOppdragsystemetTask.java
@@ -2,23 +2,16 @@ package no.nav.foreldrepenger.tilbakekreving.behandling.steg.iverksettvedtak;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.foreldrepenger.kontrakter.fpwsproxy.tilbakekreving.iverksett.TilbakekrevingVedtakDTO;
 import no.nav.foreldrepenger.tilbakekreving.behandling.beregning.BeregningsresultatTjeneste;
-import no.nav.foreldrepenger.tilbakekreving.behandling.steg.hentgrunnlag.fpwsproxy.UkjentKvitteringFraOSException;
 import no.nav.foreldrepenger.tilbakekreving.behandling.steg.hentgrunnlag.fpwsproxy.ØkonomiProxyKlient;
-import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.Behandling;
-import no.nav.foreldrepenger.tilbakekreving.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.fagsak.FagsakProsesstaskRekkefølge;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.iverksetting.OppdragIverksettingStatusRepository;
 import no.nav.foreldrepenger.tilbakekreving.behandlingslager.task.ProsessTaskDataWrapper;
 import no.nav.foreldrepenger.tilbakekreving.iverksettevedtak.tjeneste.TilbakekrevingsvedtakTjeneste;
-import no.nav.vedtak.exception.IntegrasjonException;
-import no.nav.vedtak.felles.jpa.savepoint.RunWithSavepoint;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
@@ -30,8 +23,6 @@ public class SendVedtakTilOppdragsystemetTask implements ProsessTaskHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(SendVedtakTilOppdragsystemetTask.class);
 
-    private EntityManager entityManager;
-    private BehandlingRepository behandlingRepository;
     private OppdragIverksettingStatusRepository oppdragIverksettingStatusRepository;
     private TilbakekrevingsvedtakTjeneste tilbakekrevingsvedtakTjeneste;
     private BeregningsresultatTjeneste beregningsresultatTjeneste;
@@ -43,14 +34,10 @@ public class SendVedtakTilOppdragsystemetTask implements ProsessTaskHandler {
     }
 
     @Inject
-    public SendVedtakTilOppdragsystemetTask(EntityManager entityManager,
-                                            BehandlingRepository behandlingRepository,
-                                            OppdragIverksettingStatusRepository oppdragIverksettingStatusRepository,
+    public SendVedtakTilOppdragsystemetTask(OppdragIverksettingStatusRepository oppdragIverksettingStatusRepository,
                                             BeregningsresultatTjeneste beregningsresultatTjeneste,
                                             TilbakekrevingsvedtakTjeneste tilbakekrevingsvedtakTjeneste,
                                             ØkonomiProxyKlient økonomiProxyKlient) {
-        this.entityManager = entityManager;
-        this.behandlingRepository = behandlingRepository;
         this.oppdragIverksettingStatusRepository = oppdragIverksettingStatusRepository;
         this.beregningsresultatTjeneste = beregningsresultatTjeneste;
         this.tilbakekrevingsvedtakTjeneste = tilbakekrevingsvedtakTjeneste;
@@ -60,31 +47,11 @@ public class SendVedtakTilOppdragsystemetTask implements ProsessTaskHandler {
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
         long behandlingId = ProsessTaskDataWrapper.wrap(prosessTaskData).getBehandlingId();
-        Behandling behandling = behandlingRepository.hentBehandling(behandlingId);
         beregningsresultatTjeneste.beregnOgLagre(behandlingId); //midlertidig her for å raskere kunne fase ut håndtering av lagret XML
         var tilbakekrevingsvedtak = tilbakekrevingsvedtakTjeneste.lagTilbakekrevingsvedtak(behandlingId);
-        oppdragIverksettingStatusRepository.registrerStarterIverksetting(behandlingId, tilbakekrevingsvedtak.vedtakId().toString());
-        lagSavepointOgIverksettViaFpWsProxy(behandlingId, tilbakekrevingsvedtak);
+        økonomiProxyKlient.iverksettTilbakekrevingsvedtak(tilbakekrevingsvedtak);
+        LOG.info("Tilbakekrevingsvedtak sendt OK til oppdragsystemet via fpwsproxy. BehandlingId={}", behandlingId);
+        oppdragIverksettingStatusRepository.registrerKvittertVedtak(behandlingId, tilbakekrevingsvedtak.vedtakId().toString());
     }
 
-    private void lagSavepointOgIverksettViaFpWsProxy(long behandlingId, TilbakekrevingVedtakDTO tilbakekrevingsvedtak) {
-        var runWithSavepoint = new RunWithSavepoint(entityManager);
-        runWithSavepoint.doWork(() -> {
-            //setter før kall til OS, slik at kvittering-status blir lagret selv om kallet feiler
-            try {
-                økonomiProxyKlient.iverksettTilbakekrevingsvedtak(tilbakekrevingsvedtak);
-                LOG.info("Tilbakekrevingsvedtak sendt OK til oppdragsystemet via fpwsproxy. BehandlingId={}", behandlingId);
-                oppdragIverksettingStatusRepository.registrerKvittering(behandlingId, true);
-            } catch (UkjentKvitteringFraOSException e) {
-                oppdragIverksettingStatusRepository.registrerKvittering(behandlingId, false);
-                var rwsp = new RunWithSavepoint(entityManager);
-                rwsp.doWork(() -> {
-                    // Kaster feil for å feile prosesstasken, samt trigge logging.
-                    // Setter savepoint før feilen kastes, slik at kvittering-status blir lagret, selv om det blir exception
-                    throw new IntegrasjonException("FPT-609912", String.format("Fikk feil fra OS ved iverksetting av behandlingId=%s. Sjekk loggen til fpwsproxy for mer info.", behandlingId));
-                });
-            }
-            return null;
-        });
-    }
 }
